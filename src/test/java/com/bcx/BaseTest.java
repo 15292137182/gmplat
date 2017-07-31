f8c5ada1a6933619753e0856c85fec7292e1222d
wjf <xeternity@qq.com>
2017-07-31 11:04:36 +0800

    test for space 3

--- src/test/java/com/bcx/BaseTest.java
+++ src/test/java/com/bcx/BaseTest.java
@@ -11,6 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 基础测试类,加载Spring测试环境
  * 但继承本类并不是必须的
  * 在不继承该类的情况下，若要使用spring环境，请在class上加入注解：RunWith ContextConfiguration
+ *
+ *
+ *
+ *
+ *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-application.xml", "classpath:spring/spring-mybatis.xml"})
