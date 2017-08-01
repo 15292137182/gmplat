83a2542c61beb16543d6de07c0ecae4aeabe32c3
tanshijun <453212598@qq.com>
2017-08-01 18:31:24 +0800

    数据库表，数据库表字段信息维护

---
+++ src/main/webapp/template/WebUI/DataSheet/DataBase.js
@@ -0,0 +1,54 @@
+/**
+ * Created by andim on 2017/8/1.
+ */
+    var vm = new Vue({
+        el:"#app",
+        data:{
+            input:'',
+            myData:[],
+            rightData:[],
+            leftHeight:'',
+            rightHeight:'',
+        },
+        methods:{
+            get(){
+                this.$http.jsonp('http://192.168.100.193/GMPlat/maint/select',{
+                    "str":this.input
+                },{
+                    jsonp:'callback'
+                }).then(function (res) {
+                    this.myData=res.data.content.data;
+                })
+            },
+            click(row, event, column){
+                this.FindData(row.rowId);
+                this.FindOk(row);
+            },
+            FindData(id){
+                this.$http.jsonp('http://192.168.100.193/GMPlat/maint/selectById',{
+                    "rowId":id
+                },{
+                    jsonp:'callback'
+                }).then(function (res) {
+                    this.rightData=res.data.content.data;
+                })
+            },
+            FindOk(row){
+                    this.$refs.myTable.setCurrentRow(row);
+            }
+        },
+        created(){
+            this.get();
+            $(document).ready(function(){
+                vm.leftHeight=$(window).height()-120;
+                vm.rightHeight=$(window).height()-120;
+            });
+            $(window).resize(function(){
+                vm.leftHeight=$(window).height()-120;
+                vm.rightHeight=$(window).height()-120;
+            })
+        },
+        updated(){
+
+        }
+    });
