1bd1b8be1d608c88d3046dfb510ba9b4852213be
HCL <825944942@qq.com>
2017-08-01 18:48:47 +0800

    修正

--- src/test/java/com/bcx/plat/core/controller/BusinessObjectControllerTest.java
+++ src/test/java/com/bcx/plat/core/controller/BusinessObjectControllerTest.java
@@ -1,10 +1,15 @@
 package com.bcx.plat.core.controller;
 
 import com.bcx.BaseTest;
+import org.junit.Test;
 
 /**
  * Created by Went on 2017/8/1.
  */
 public class BusinessObjectControllerTest extends BaseTest{
 
+    @Test
+    public void test(){
+
+    }
 }
