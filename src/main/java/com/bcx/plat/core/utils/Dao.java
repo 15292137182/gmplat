fa96778212269a663d9b01a7a639f2fc74a7c228
HCL <825944942@qq.com>
2017-08-02 21:01:17 +0800

    google 风格规范代码

--- src/main/java/com/bcx/plat/core/utils/Dao.java
+++ src/main/java/com/bcx/plat/core/utils/Dao.java
@@ -4,19 +4,17 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 
 /**
- * Dao 操作，该操作方式将在不久的将来被废弃
- * <p>
- * dao.getMapper(EntityMapper.class).insert(Entity entity)
+ * Dao 操作，该操作方式将在不久的将来被废弃 <p> dao.getMapper(EntityMapper.class).insert(Entity entity)
  */
 @Deprecated
 public class Dao extends SqlSessionTemplate {
 
-    /**
-     * 依赖注入的方法
-     *
-     * @param sqlSessionFactory 需要注入sqlSession 工厂
-     */
-    public Dao(SqlSessionFactory sqlSessionFactory) {
-        super(sqlSessionFactory);
-    }
+  /**
+   * 依赖注入的方法
+   *
+   * @param sqlSessionFactory 需要注入sqlSession 工厂
+   */
+  public Dao(SqlSessionFactory sqlSessionFactory) {
+    super(sqlSessionFactory);
+  }
 }
