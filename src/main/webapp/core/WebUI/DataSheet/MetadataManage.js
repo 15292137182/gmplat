3298bbbfb371d484eb7ce9c0c37306f454f7349a
Zhang Yangmei <zhangyangmei@batchsight.com>
2017-08-03 20:25:52 +0800

    代码上传

--- src/main/webapp/core/WebUI/DataSheet/MetadataManage.js
+++ src/main/webapp/core/WebUI/DataSheet/MetadataManage.js
@@ -1,51 +1,12 @@
 /**
  * Created by admin on 2017/8/1.
  */
-var vm = new Vue({
-  el: "#app",
-  data: {
-    input: '',
-    leftHeight: '',
-    rightHeight: '',
-    myLeftData: [],
-    myRightData: []
+var basTop=new Vue({
+  el:'#basTop',
+  data:{
+
   },
-  methods: {
-    searchTable() {
-      this.$http.jsonp(serverPath + "/businObj/select", {
-        str: this.input
-      }, {
-        jsonp: 'callback'
-      }).then(function (res) {
-        //清空表格
-        this.myLeftData = [];
-        for (var i = 0; i < res.data.content.data.length; i++) {
-          var obj = {};
-          obj.data = res.data.content.data[i];
-          this.myLeftData.push(obj.data);
-        }
-      });
-    },
-    handleCurrentChange(val) {
-      //点击拿到这条数据的值
-      this.currentVal = val;
-      var currentId = val.rowId;
-      //查找右侧表的数据
-      this.$http.jsonp(serverPath + "/businObjPro/select", {
-        rowId: currentId
-      }, {
-        jsonp: 'callback'
-      }).then(function (res) {
-        console.log(res.data.content.data);
-        //清空表格
-        this.myRightData = [];
-        for (var i = 0; i < res.data.content.data.length; i++) {
-          var obj = {};
-          obj.data = res.data.content.data[i];
-          this.myRightData.push(obj.data);
-        }
-      });
-    },
+  methods:{
     addEvent() {
       operate = 1;
       var htmlUrl = 'MetadataAdd.html';
@@ -54,40 +15,135 @@ var vm = new Vue({
     editEvent() {
       operate = 2;
       var htmlUrl = 'MetadataAdd.html';
-      divIndex = ibcpLayer.ShowDiv(htmlUrl, '编辑业务对象', '400px', '340px',
-          function () {
+      divIndex = ibcpLayer.ShowDiv(htmlUrl, '编辑业务对象', '400px', '340px', function () {
             //code值
-            em.codeInput = vm.currentVal.objectCode;
+            em.codeInput = basLeft.currentVal.objectCode;
             //不可点击
             em.disabled = true;
-            em.nameInput = vm.currentVal.objectName;
-            em.tableInput = vm.currentVal.tables;
-            em.versionInput = vm.currentVal.relateTableRowId;
-            em.dataId = vm.currentVal.proRowId;
+            em.firstIconEvent = true;
+            em.nameInput = basLeft.currentVal.objectName;
+            em.tableInput = basLeft.currentVal.tables;
+            em.versionInput = basLeft.currentVal.relateTableRowId;
+            em.dataId = basLeft.currentVal.proRowId;
           });
     },
     deleteEvent() {
-      var deleteId = vm.currentVal.rowId;
+      var deleteId = basLeft.currentVal.rowId;
       console.log(deleteId);
       this.$http.jsonp(serverPath + "/businObj/delete", {
         rowId: deleteId
       }, {
         jsonp: 'callback'
-      }).then(function () {
-        ibcpLayer.ShowOK('删除成功');
-        this.searchTable();
+      }).then(function (ref) {
+        console.log(ref);
+        ibcpLayer.ShowOK(ref.data.message);
+        basLeft.searchLeftTable();
       });
+    },
+    addProp(){
+      var htmlUrl = 'MetadataPropAdd.html';
+      divIndex = ibcpLayer.ShowDiv(htmlUrl, '新增对象属性', '400px', '440px',function(){
+          this.$http.jsonp()
+      });
+    },
+    editProp(){
+
+    },
+    affectProp(){
+
+    },
+    changeProp(){
+
+    }
+  }
+});
+
+var basLeft=new Vue({
+  "el":"#basLeft",
+   data:{
+    leftInput: '',
+    leftHeight: '',
+    myLeftData: []
+  },
+  methods:{
+    searchLeftTable() {
+      this.$http.jsonp(serverPath + "/businObj/select", {
+        str: this.leftInput
+      }, {
+        jsonp: 'callback'
+      }).then(function (res) {
+        if(res.data.content.data!==''){
+          console.log(res);
+          this.myLeftData = res.data.content.data;
+          //默认查找第一行右边的数据
+          this.currentChange(this.myLeftData[0])
+        }
+      });
+    },
+    currentChange(row, event, column) {
+      //点击拿到这条数据的值
+      //console.log(row)
+      this.currentVal = row;
+      this.currentId = row.rowId;
+      //查找右侧表的数据
+      this.$http.jsonp(serverPath + "/businObjPro/select", {
+        rowId: this.currentId
+      }, {
+        jsonp: 'callback'
+      }).then(function (res) {
+          basRight.myRightData = res.data.content.data;
+      });
+    },
+    FindFirstDate(row){
+      this.$refs.myLeftData.setCurrentRow(row);
     }
   },
   created() {
-    this.searchTable();
+    this.searchLeftTable();
     $(document).ready(function () {
-      vm.leftHeight = $(window).height() - 165;
-      vm.rightHeight = $(window).height() - 120;
+      basLeft.leftHeight = $(window).height() - 154;
     });
     $(window).resize(function () {
-      vm.leftHeight = $(window).height() - 165;
-      vm.rightHeight = $(window).height() - 120;
+      basLeft.leftHeight = $(window).height() - 154;
     })
+  },
+  updated() {
+    this.FindFirstDate(this.myLeftData[0]);
+  }
+});
+
+var basRight=new Vue({
+  "el":"#basRight",
+  data:{
+    rightInput: '',
+    rightHeight: '',
+    myRightData: []
+  },
+  methods:{
+    searchRightTable() {
+      this.$http.jsonp(serverPath + "/businObjPro/select", {
+        str: this.rightInput,
+        rowId:basLeft.currentId
+      }, {
+        jsonp: 'callback'
+      }).then(function (res) {
+        basRight.myRightData= res.data.content.data;
+      });
+    },
+    FindFirstDate(row){
+      this.$refs.myRightData.setCurrentRow(row);
+    }
+  },
+  created() {
+    this.searchRightTable();
+    $(document).ready(function () {
+      basRight.rightHeight = $(window).height() - 154;
+    });
+    $(window).resize(function () {
+      basRight.rightHeight = $(window).height() - 154;
+    })
+  },
+  updated() {
+    this.FindFirstDate(this.myRightData[0]);
   }
 })
--- src/main/webapp/core/WebUI/Public/publicjs.js
+++ src/main/webapp/core/WebUI/Public/publicjs.js
@@ -1,70 +1,45 @@
 var path = getRootPath();
 
-document.write('<link href="' + path
-    + '/webjar/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">');
-document.write('<link href="' + path
-    + '/webjar/css/mystyle.css" rel="stylesheet">');
+document.write('<link href="' + path + '/webjar/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">');
+document.write('<link href="' + path + '/webjar/css/mystyle.css" rel="stylesheet">');
+document.write('<link href="' + path + '/WebUI/element-ui/lib/theme-default/index.css" rel="stylesheet">');
+//自定义样式
+document.write('<link href="' + path + '/webjar/css/coreStyle.css" rel="stylesheet">');
 
-document.write('<link href="' + path
-    + '/WebUI/element-ui/lib/theme-default/index.css" rel="stylesheet">');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/vue/dist/vue.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/element-ui/lib/index.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/vue/vue-resource.js" charset="UTF-8"></script>');
 
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/vue/dist/vue.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/element-ui/lib/index.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/vue/vue-resource.js" charset="UTF-8"></script>');
-
-document.write('<script type="text/javascript" src="' + path
-    + '/webjar/jquery/3.1.1/jquery.min.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/webjar/layer/layer.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/webjar/plugins/layui/layui.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/sha256.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ServerPath.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcp.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/cookie_util.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpLayer.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpAjax.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/webjar/jquery/3.1.1/jquery.min.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/webjar/layer/layer.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/webjar/plugins/layui/layui.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/sha256.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ServerPath.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcp.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/cookie_util.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpLayer.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpAjax.js" charset="UTF-8"></script>');
 // document.write('<script type="text/javascript" src="' + path +  '/WebUI/Public/ibcpSign.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpSignature.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpSignature.js" charset="UTF-8"></script>');
 //document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpPlugin.js" charset="UTF-8"></script>');
 //document.write('<script type="text/javascript" src="' + path + '/WebUI/Version/ibcpVS.js" charset="UTF-8"></script>');
 //document.write('<script type="text/javascript" src="' + path + '/WebUI/Uom/UomChose.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/DateFormat.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpValidate.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/NumberKeyBoard.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/DateFormat.js" charset="UTF-8"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpValidate.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/NumberKeyBoard.js"></script>');
 // document.write('<script type="text/javascript" src="' + path + '/WebUI/PV/Chart/highcharts.js" charset="UTF-8"></script>');
 // document.write('<script type="text/javascript" src="' + path + '/WebUI/PV/Chart/exporting.js" charset="UTF-8"></script>');
 // document.write('<script type="text/javascript" src="' + path + '/WebUI/PV/Chart/highcharts-zh_CN.js" charset="UTF-8"></script>')
 //document.write('<script type="text/javascript" src="' + path + '/WebUI/PV/RevisePV.js" charset="UTF-8"></script>');
 //document.write('<script type="text/javascript" src="' + path + '/WebUI/PV/PV_Edit/PV_DateTime_Edit.js" charset="UTF-8"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpAreaResize.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpAuthority.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpDate.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpSelect.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpCheck.js"></script>');
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpAfter.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpAreaResize.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpAuthority.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpDate.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpSelect.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpCheck.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpAfter.js"></script>');
 
-document.write('<script type="text/javascript" src="' + path
-    + '/WebUI/Public/ibcpKeyboard.js"></script>');
+document.write('<script type="text/javascript" src="' + path + '/WebUI/Public/ibcpKeyboard.js"></script>');
 
 function getRootPath() {
   //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
