7ba6ef33937377b7f01f556aefe691b35d86a617
wth <wentiehu@batchsight.com>
2017-08-06 09:57:06 +0800

    功能块属性

--- src/main/java/com/bcx/plat/core/service/Impl/BusinessObjectServiceImpl.java
+++ src/main/java/com/bcx/plat/core/service/Impl/BusinessObjectServiceImpl.java
@@ -6,6 +6,7 @@ import static com.bcx.plat.core.base.BaseConstants.TAKE_EFFECT;
 import static com.bcx.plat.core.base.BaseConstants.UNAVAILABLE;
 import static com.bcx.plat.core.constants.Message.*;
 
+import com.bcx.plat.core.base.BaseService;
 import com.bcx.plat.core.entity.BusinessObject;
 import com.bcx.plat.core.entity.BusinessObjectPro;
 import com.bcx.plat.core.mapper.BusinessObjectMapper;
@@ -16,114 +17,123 @@ import com.bcx.plat.core.utils.ServiceResult;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 业务对象业务层 Created by Went on 2017/8/1.
+ * 业务对象业务层
+ * <p>
+ * Created by Went on 2017/8/1.
  */
 @Service
 @Transactional
-public class BusinessObjectServiceImpl implements BusinessObjectService {
+public class BusinessObjectServiceImpl extends BaseService implements BusinessObjectService {
 
-  @Autowired
-  private BusinessObjectMapper businessObjectMapper;
-  @Autowired
-  private BusinessObjectProMapper businessObjectProMapper;
-  /**
-   * 查询业务对象 输入空格分隔的查询关键字（对象代码、对象名称、关联表）
-   */
-  @Override
-  public ServiceResult<BusinessObject> select(Map map) {
-    ServiceResult<BusinessObject> result = null;
-      if (map.size() != 0) {
-        List<BusinessObject> select = businessObjectMapper.select(map);
-        for (int i = 0; i < select.size(); i++) {
-          String tableCname = select.get(i).getTableCname();
-          String tableSchema = select.get(i).getTableSchema();
-          String string = tableSchema + "(" + tableCname + ")";
-          select.get(i).setTables(string);
-          result = new ServiceResult<>(QUERY_SUCCESS,select);
+    @Autowired
+    private BusinessObjectMapper businessObjectMapper;
+    @Autowired
+    private BusinessObjectProMapper businessObjectProMapper;
+
+    /**
+     * 查询业务对象 输入空格分隔的查询关键字（对象代码、对象名称、关联表）
+     */
+    @Override
+    public ServiceResult<BusinessObject> select(Map map) {
+        ServiceResult<BusinessObject> result = null;
+        if (map.size() != 0) {
+            List<BusinessObject> select = businessObjectMapper.select(map);
+            for (int i = 0; i < select.size(); i++) {
+                String tableCname = select.get(i).getTableCname();
+                String tableSchema = select.get(i).getTableSchema();
+                String string = tableSchema + "(" + tableCname + ")";
+                select.get(i).setTables(string);
+                logger.info(QUERY_SUCCESS);
+                result = new ServiceResult<>(QUERY_SUCCESS, select);
+            }
+            if (select.size() == 0) {
+                result = new ServiceResult<>(QUERY_FAIL, "");
+            }
         }
-        if(select.size()==0){
-          result = new ServiceResult<>(QUERY_FAIL,"");
+        return result;
+    }
+
+    /**
+     * 新增业务对象录入框，包括：对象代码，对象名称，关联表(单选)，版本(系统生成)
+     *
+     * @param businessObject
+     * @return
+     */
+    @Override
+    public ServiceResult<BusinessObject> insert(BusinessObject businessObject) {
+        try {
+            //业务对象版本号默认从1.0开始
+            businessObject.setVersion("1.0");
+            businessObject.buildCreateInfo();
+            //新增数默认状态为不可用
+            businessObject.setStatus(UNAVAILABLE);
+            String rowId = businessObject.getRowId();
+            businessObjectMapper.insert(businessObject);
+            //将用户新增的rowId返回
+            return new ServiceResult<>(NEW_ADD_SUCCESS, rowId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ServiceResult<>(NEW_ADD_FAIL, "");
         }
-      }
-      return result;
-  }
-
-  /**
-   * 新增业务对象录入框，包括：对象代码，对象名称，关联表(单选)，版本(系统生成)
-   */
-  @Override
-  public ServiceResult<BusinessObject> insert(BusinessObject businessObject) {
-    try {
-      //业务对象版本号默认从1.0开始
-      businessObject.setVersion("1.0");
-      businessObject.buildCreateInfo();
-      //新增数默认状态为不可用
-      businessObject.setStatus(UNAVAILABLE);
-      String rowId = businessObject.getRowId();
-      businessObjectMapper.insert(businessObject);
-      //将用户新增的rowId返回
-      return new ServiceResult<>(NEW_ADD_SUCCESS, rowId);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return new ServiceResult<>(NEW_ADD_FAIL, "");
     }
-  }
 
-  /**
-   * 编辑对象名称字段
-   */
-  @Override
-  public ServiceResult<BusinessObject> update(BusinessObject businessObject) {
-    try {
-      businessObject.buildModifyInfo();
-      businessObjectMapper.update(businessObject);
-      String rowId = businessObject.getRowId();
-      return new ServiceResult<>(UPDATE_SUCCESS, rowId);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return new ServiceResult<>(UPDATE_FAIL, "");
-    }
-  }
-
-  /**
-   * 删除业务对象
-   */
-  @Override
-  public ServiceResult<BusinessObject> delete(String rowId) {
-    try {
-      Map<String,Object> map = new HashMap<>();
-      map.put("rowId", rowId);
-      List<BusinessObjectPro> select = businessObjectProMapper.select(null);
-      for (int i = 0; i <select.size(); i++) {
-        if (select.get(i).getRelateTableColumn()==rowId){
-          businessObjectProMapper.deleteRelateCol(map);
+    /**
+     * 编辑对象名称字段
+     */
+    @Override
+    public ServiceResult<BusinessObject> update(BusinessObject businessObject) {
+        try {
+            businessObject.buildModifyInfo();
+            businessObjectMapper.update(businessObject);
+            String rowId = businessObject.getRowId();
+            return new ServiceResult<>(UPDATE_SUCCESS, rowId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ServiceResult<>(UPDATE_FAIL, "");
         }
-      }
-      businessObjectMapper.delete(rowId);
-      return new ServiceResult<>(STATUS_SUCCESS, DELETE_SUCCESS, "");
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-    return new ServiceResult<>(STATUS_FAIL, DELETE_FAIL, "");
-  }
 
-  /**
-   * 获取ID对该条记录执行变更,没有生效的不能执行变更
-   */
-  @Override
-  public ServiceResult updateExecuChange(String rowId) {
-    BusinessObject select = businessObjectMapper.selectById(rowId);
-    String status = select.getStatus();
-    if (!(status == TAKE_EFFECT)) {
-      return new ServiceResult("状态没有生效,不能执行变更", "");
-    } else {
-      businessObjectMapper.updateExecuChange(rowId);
-      return new ServiceResult();
+    /**
+     * 删除业务对象
+     */
+    @Override
+    public ServiceResult<BusinessObject> delete(String rowId) {
+        try {
+            Map<String, Object> map = new HashMap<>();
+            map.put("rowId", rowId);
+            List<BusinessObjectPro> select = businessObjectProMapper.select(null);
+            for (int i = 0; i < select.size(); i++) {
+                String relateTableColumn = select.get(i).getRelateTableColumn();
+                if (relateTableColumn.equals(rowId)) {
+                    businessObjectProMapper.deleteRelateCol(map);
+                }
+            }
+            businessObjectMapper.delete(rowId);
+            return new ServiceResult<>(STATUS_SUCCESS, DELETE_SUCCESS, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ServiceResult<>(STATUS_FAIL, DELETE_FAIL, "");
     }
-  }
+
+    /**
+     * 获取ID对该条记录执行变更,没有生效的不能执行变更
+     */
+    @Override
+    public ServiceResult updateExecuChange(String rowId) {
+        BusinessObject select = businessObjectMapper.selectById(rowId);
+        String status = select.getStatus();
+        if (!(status == TAKE_EFFECT)) {
+            return new ServiceResult("状态没有生效,不能执行变更", "");
+        } else {
+            businessObjectMapper.updateExecuChange(rowId);
+            return new ServiceResult();
+        }
+    }
 }
---
+++ src/main/java/com/bcx/plat/core/service/Impl/FrontFuncProServiceImpl.java
@@ -0,0 +1,82 @@
+package com.bcx.plat.core.service.Impl;
+
+import com.bcx.plat.core.entity.FrontFuncPro;
+import com.bcx.plat.core.mapper.FrontFuncProMapper;
+import com.bcx.plat.core.service.FrontFuncProService;
+import com.bcx.plat.core.utils.ServiceResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.bcx.plat.core.constants.Message.*;
+
+/**
+ * Created by Wen Tiehu on 2017/8/4.
+ */
+@Service
+public class FrontFuncProServiceImpl implements FrontFuncProService {
+
+    @Autowired
+    private FrontFuncProMapper frontFuncProMapper;
+    /**
+     * 查询前端功能块属性项信息
+     *
+     * @param map 接受传入进来的map参数
+     * @return 返回查询数据以及状态
+     */
+    @Override
+    public ServiceResult queryFronFuncPro(Map map) {
+        List<FrontFuncPro> result = frontFuncProMapper.select(map);
+        if (result.size()==0){
+            return new ServiceResult(QUERY_FAIL,"");
+        }
+        return new ServiceResult(QUERY_SUCCESS, result);
+    }
+
+    /**
+     * 新增前端功能块属性项信息维护
+     *
+     * @param entity 前端功能块属性实体
+     * @return 返回新增状态
+     */
+    @Override
+    public ServiceResult addFronFuncPro(FrontFuncPro entity) {
+        entity.buildCreateInfo();
+        String rowId = entity.getRowId();
+        int result = frontFuncProMapper.insert(entity);
+        if (result!=1){
+            return new ServiceResult(NEW_ADD_FAIL,"");
+        }
+        return new ServiceResult(NEW_ADD_SUCCESS,rowId);
+    }
+
+    /**
+     * 修改前端功能块属性项信息维护
+     *
+     * @param entity 前端功能块属性实体
+     * @return 返回修改状态
+     */
+    @Override
+    public ServiceResult modifyFronFuncPro(FrontFuncPro entity) {
+        entity.buildModifyInfo();
+        int update = frontFuncProMapper.update(entity);
+        if(update != 1){
+            return  new ServiceResult<>(UPDATE_FAIL ,"");
+        }
+        return  new ServiceResult<>(UPDATE_SUCCESS ,"");
+    }
+
+    /**
+     * 删除前端功能块属性项信息维护
+     *
+     * @param delData 接受需要删除数据的id
+     * @return 返回删除的状态
+     */
+    @Override
+    public ServiceResult delete(String delData) {
+        frontFuncProMapper.delete(delData);
+        return new ServiceResult<>(DELETE_SUCCESS,"");
+    }
+}
