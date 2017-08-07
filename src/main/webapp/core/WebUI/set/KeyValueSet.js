2dac3ac50d1d9386a49b83d2b221be8472a012e8
jimishu <243047281@qq.com>
2017-08-07 10:17:40 +0800

    。。

--- src/main/webapp/core/WebUI/set/KeyValueSet.js
+++ src/main/webapp/core/WebUI/set/KeyValueSet.js
@@ -1,3 +1,27 @@
 /**
  * Created by jms on 2017/8/6.
  */
+
+var vm=new Vue({
+    el:"#kvsInfo",
+    data:{
+        keyValueSetdata:[],
+        input:'',
+    },
+    methods:{
+        search(){
+            this.$http.jsonp(serverPath+"/keySet/select",{
+                "str":this.input
+            },{
+                jsonp:'callback'
+            }).then(function(res){
+                this.keyValueSetdata=res.data.content.data;
+            });
+        }
+    },
+    created(){
+        this.search();
+    },
+
+
+})
