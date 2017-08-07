344fab2d627f44e06180c41a8fb099bd5c842902
HCL <825944942@qq.com>
2017-08-07 16:18:32 +0800

    提交BaseService和BaseController

--- src/main/java/com/bcx/plat/core/base/BaseController.java
+++ src/main/java/com/bcx/plat/core/base/BaseController.java
@@ -1,5 +1,11 @@
 package com.bcx.plat.core.base;
 
+import static com.bcx.plat.core.utils.UtilsTool.isValid;
+import static com.bcx.plat.core.utils.UtilsTool.objToJson;
+
+import com.bcx.plat.core.utils.ServiceResult;
+import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +26,26 @@ public class BaseController {
    */
   @Autowired
   protected ResourceBundleMessageSource messageSource;
+
+  /**
+   * 对返回的结果进行处理
+   *
+   * @param request 请求
+   * @param serviceResult 结果信息
+   * @param locale 国际化
+   * @return 返回
+   */
+  protected Object result(HttpServletRequest request, ServiceResult serviceResult, Locale locale) {
+    if (null != serviceResult) {
+      String message = messageSource.getMessage(serviceResult.getMessage(), null, locale);
+      if (isValid(message)) {
+        serviceResult.setMessage(message);
+      }
+      if (isValid(request.getParameter("callback"))) {
+        StringBuilder sb = new StringBuilder(request.getParameter("callback"));
+        return sb.append("(").append(objToJson(serviceResult)).append(")");
+      }
+    }
+    return serviceResult;
+  }
 }
--- src/main/java/com/bcx/plat/core/base/BaseService.java
+++ src/main/java/com/bcx/plat/core/base/BaseService.java
@@ -1,28 +1,13 @@
 package com.bcx.plat.core.base;
 
-
-import com.bcx.plat.core.utils.ServiceResult;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Create By HCL at 2017/8/1
  */
-public interface BaseService<T extends BaseEntity> {
+public class BaseService {
 
-  /**
-   * 查询方法
-   *
-   * @param map 查询参数
-   * @param page 是否分页
-   * @return 返回
-   */
-  ServiceResult select(Map map, int page, int limit);
+  protected Logger logger = LoggerFactory.getLogger(getClass());
 
-  ServiceResult selectList(Map map);
-
-  ServiceResult update(Map map);
-
-  ServiceResult insert(Map map);
-
-  ServiceResult delete(Map map);
 }
