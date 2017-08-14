52a192b9e11e2919964344c5d2d69c63f526f2ee
HCL <825944942@qq.com>
2017-08-14 18:37:22 +0800

    去除SequenceService的Setter方法

--- src/main/java/com/bcx/plat/core/controller/SequenceRuleConfigController.java
+++ src/main/java/com/bcx/plat/core/controller/SequenceRuleConfigController.java
@@ -4,14 +4,18 @@ package com.bcx.plat.core.controller;
 import static com.bcx.plat.core.base.BaseConstants.STATUS_FAIL;
 import static com.bcx.plat.core.base.BaseConstants.STATUS_SUCCESS;
 import static com.bcx.plat.core.utils.UtilsTool.isValid;
+import static com.bcx.plat.core.utils.UtilsTool.jsonToObj;
 
 import com.bcx.plat.core.common.BaseControllerTemplate;
 import com.bcx.plat.core.entity.SequenceRuleConfig;
+import com.bcx.plat.core.manager.SequenceManager;
 import com.bcx.plat.core.service.SequenceRuleConfigService;
 import com.bcx.plat.core.utils.ServiceResult;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,12 +39,22 @@ public class SequenceRuleConfigController extends
    * @param request 请求
    * @return 返回流水号的生成列表
    */
+  @RequestMapping("/mock")
   public Object mockSequenceNo(HttpServletRequest request, Locale locale) {
     String _content = request.getParameter("content");
     ServiceResult _sr = new ServiceResult();
     if (isValid(_content)) {
       _sr.setState(STATUS_SUCCESS);
-      List<String> s = null;
+      String _args = request.getParameter("args");
+      Map<String, Object> map = new HashMap<>();
+      try {
+        map = jsonToObj(_args, Map.class);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      String _num = request.getParameter("num");
+      int num = _num.matches("^\\d+$") ? Integer.parseInt(_num) : 5;
+      List<String> strings = SequenceManager.getInstance().mockSequenceNo(_content, map, num);
       _sr.setMessage("OPERATOR_SUCCESS");
 
     } else {
