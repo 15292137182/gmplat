ecaabef74defb79aefce9dbe3ea5ed9e2211a104
tanshijun <453212598@qq.com>
2017-08-14 15:46:45 +0800

    功能块画面分页查询，删除提示框方法

--- src/main/webapp/core/webjar/plugins/js/TabPage.js
+++ src/main/webapp/core/webjar/plugins/js/TabPage.js
@@ -27,7 +27,53 @@ var pagingObj = (function(){
             }
         })
     }
+    var Examples = function(url,rowId,args,pageSize,pageNum,obj,callback){
+        $.ajax({
+            url:url,
+            type:"get",
+            data:{
+                rowId:rowId,
+                args:args,
+                pageSize:pageSize,
+                pageNum:pageNum
+            },
+            dataType:"jsonp",
+            success:function(res){
+                obj.loading=false;
+                if(res.data.result.length!=0){
+                    obj.tableData = res.data.result;//数据源
+                    obj.allDate = Number(res.data.total);//总共多少条数据
+                    if(typeof callback =="function"){
+                            callback(res);
+                        }
+                }else{
+                    obj.tableData = [];
+                }
+                // if(typeof callback =="function"){
+                //     callback(res);
+                // }
+            },
+            error:function(){
+                obj.loading=false;
+                alert("错误")
+            }
+        })
+    }
     return {
-        Example:Example
+        Example:Example,
+        Examples:Examples
+    }
+})()
+
+var deleteObj = (function(){
+    var del = function(callback){
+        ibcpLayer.ShowConfirm("您确定删除吗?",function(){
+            if(typeof callback =="function"){
+                callback();
+            }
+        })
+    }
+    return {
+        del:del
     }
 })()
