b40ca312e34895aaa5fbea644f05d93859ad6c6a
wth <wentiehu@batchsight.com>
2017-08-14 15:09:17 +0800

    序列规则重构

--- src/main/java/com/bcx/plat/core/controller/SequenceRuleConfigController.java
+++ src/main/java/com/bcx/plat/core/controller/SequenceRuleConfigController.java
@@ -1,23 +1,28 @@
 package com.bcx.plat.core.controller;
 
+import static com.bcx.plat.core.utils.UtilsTool.collectToSet;
+
+import com.bcx.plat.core.base.BaseController;
 import com.bcx.plat.core.common.BaseControllerTemplate;
 import com.bcx.plat.core.entity.SequenceRuleConfig;
+
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+
 import com.bcx.plat.core.service.SequenceRuleConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Create By HCL at 2017/8/8
  */
 @RestController
 @RequestMapping("/core/sequenceRule")
-public class SequenceRuleConfigController extends BaseControllerTemplate<SequenceRuleConfigService, SequenceRuleConfig> {
+public class SequenceRuleConfigController extends BaseControllerTemplate<SequenceRuleConfigService,SequenceRuleConfig> {
 
-    @Override
-    protected List<String> blankSelectFields() {
-        return Arrays.asList("seqCode", "seqName", "seqContent", "desp");
-    }
+  @Override
+  protected List<String> blankSelectFields() {
+    return Arrays.asList("seqCode","seqName","seqContent","desp");
+  }
 }
--- src/main/java/com/bcx/plat/core/service/SequenceRuleConfigService.java
+++ src/main/java/com/bcx/plat/core/service/SequenceRuleConfigService.java
@@ -2,17 +2,11 @@ package com.bcx.plat.core.service;
 
 import com.bcx.plat.core.common.BaseServiceTemplate;
 import com.bcx.plat.core.entity.SequenceRuleConfig;
-import com.bcx.plat.core.utils.ServiceResult;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 /**
- * 序列号规则的服务类
- *
- * Create By HCL at 2017/8/6
+ * Created by Wen Tiehu on 2017/8/14.
  */
 @Service
-public class SequenceRuleConfigService extends BaseServiceTemplate<SequenceRuleConfig> {
-
+public class SequenceRuleConfigService extends BaseServiceTemplate<SequenceRuleConfig>{
 }
