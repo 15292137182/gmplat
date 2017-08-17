24022dd1b2486ebea70d0f0b2be6a4ed4b1b847b
HCL <825944942@qq.com>
2017-08-17 11:02:45 +0800

    加入密码工具

---
+++ src/main/java/com/bcx/plat/core/utils/HexUtil.java
@@ -0,0 +1,17 @@
+package com.bcx.plat.core.utils;
+
+/**
+ * 加密工具
+ *
+ * Create By HCL at 2017/8/17
+ */
+public class HexUtil {
+
+  /**
+   * 禁止使用 new 的方式构造类
+   */
+  private HexUtil() {
+  }
+
+
+}
