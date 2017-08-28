5bd2d8f321c0dabb0083369a36ed64f130a01c92
HCL <825944942@qq.com>
2017-08-28 10:08:49 +0800

    jackson增肌忽略字段支持，在有不能识别的字段时仍能够转为javaBean

--- src/main/java/com/bcx/plat/core/utils/JacksonAdapter.java
+++ src/main/java/com/bcx/plat/core/utils/JacksonAdapter.java
@@ -1,12 +1,13 @@
 package com.bcx.plat.core.utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * 处理:
- *
+ * <p>
  * 1、springMVC 在处理数字类型的时候造成的精度丢失问题
  */
 public class JacksonAdapter extends ObjectMapper {
@@ -16,7 +17,8 @@ public class JacksonAdapter extends ObjectMapper {
     SimpleModule simpleModule = new SimpleModule();
     simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
     simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-    this.registerModule(simpleModule);
+    registerModule(simpleModule);
+    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
 }
