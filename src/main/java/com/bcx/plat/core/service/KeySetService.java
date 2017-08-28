c108a4c2dda3d9c90fb2f498cc416fb8af391de0
wth <wentiehu@batchsight.com>
2017-08-28 15:22:26 +0800

    键值集合service重构

--- src/main/java/com/bcx/plat/core/service/KeySetService.java
+++ src/main/java/com/bcx/plat/core/service/KeySetService.java
@@ -4,11 +4,15 @@ import com.bcx.plat.core.base.BaseConstants;
 import com.bcx.plat.core.common.BaseServiceTemplate;
 import com.bcx.plat.core.constants.Message;
 import com.bcx.plat.core.entity.KeySet;
+import com.bcx.plat.core.morebatis.app.MoreBatis;
+import com.bcx.plat.core.morebatis.command.QueryAction;
 import com.bcx.plat.core.morebatis.component.FieldCondition;
 import com.bcx.plat.core.morebatis.component.constant.Operator;
 import com.bcx.plat.core.utils.PlatResult;
 import com.bcx.plat.core.utils.ServiceResult;
 import com.bcx.plat.core.utils.UtilsTool;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.QEncoderStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,6 +23,9 @@ import java.util.*;
  */
 @Service
 public class KeySetService extends BaseServiceTemplate<KeySet> {
+
+    @Autowired
+    private MoreBatis moreBatis;
 
     /**
      * 根据编号number查询，以数组的形式传入数据进来["demo","test"]
@@ -31,7 +38,11 @@ public class KeySetService extends BaseServiceTemplate<KeySet> {
         for (Object li : list) {
             List lists = new ArrayList();
             lists.add(li);
-            List<Map<String, Object>> result = singleInputSelect(Arrays.asList("number"), lists);
+            List<Map<String, Object>> result = moreBatis.selectStatement()
+                    .select(Arrays.asList(QueryAction.ALL_FIELD))
+                    .from(moreBatis.getTable(KeySet.class))
+                    .where(UtilsTool.createBlankQuery(Arrays.asList("number"), lists))
+                    .execute();
             map.put(li, result);
         }
         String toJson = UtilsTool.objToJson(map);
@@ -47,7 +58,10 @@ public class KeySetService extends BaseServiceTemplate<KeySet> {
      * @return
      */
     public ServiceResult queryNumber(String search){
-        List<Map<String, Object>> result = select(new FieldCondition("number", Operator.EQUAL, search));
+        List<Map<String, Object>> result = moreBatis.selectStatement().select(Arrays.asList(QueryAction.ALL_FIELD))
+                .from(moreBatis.getTable(KeySet.class))
+                .where(new FieldCondition("number", Operator.EQUAL, search))
+                .execute();
         if (result.size() == 0) {
             return ServiceResult.Msg(PlatResult.Msg(BaseConstants.STATUS_FAIL, Message.QUERY_FAIL));
         }
