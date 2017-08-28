1f35a4493da030b131c4ee320eef7feada941bdb
wth <wentiehu@batchsight.com>
2017-08-28 20:34:20 +0800

    模板对象根据rowId查询模板对象属性

--- src/main/java/com/bcx/plat/core/controller/TemplateObjectController.java
+++ src/main/java/com/bcx/plat/core/controller/TemplateObjectController.java
@@ -1,13 +1,27 @@
 package com.bcx.plat.core.controller;
 
+import com.bcx.plat.core.base.BaseConstants;
 import com.bcx.plat.core.common.BaseControllerTemplate;
+import com.bcx.plat.core.constants.Message;
+import com.bcx.plat.core.entity.BusinessObjectPro;
 import com.bcx.plat.core.entity.TemplateObject;
+import com.bcx.plat.core.entity.TemplateObjectPro;
+import com.bcx.plat.core.morebatis.builder.ConditionBuilder;
+import com.bcx.plat.core.morebatis.cctv1.PageResult;
+import com.bcx.plat.core.morebatis.command.QueryAction;
+import com.bcx.plat.core.morebatis.component.Order;
+import com.bcx.plat.core.service.TemplateObjectProService;
 import com.bcx.plat.core.service.TemplateObjectService;
+import com.bcx.plat.core.utils.PlatResult;
+import com.bcx.plat.core.utils.ServiceResult;
+import com.bcx.plat.core.utils.UtilsTool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * <p>Title: TemplateObjectController</p>
@@ -29,4 +43,51 @@ public class TemplateObjectController extends BaseControllerTemplate<TemplateObj
     protected List<String> blankSelectFields() {
         return Arrays.asList("templateCode","templateCode","templateName");
     }
+
+    @Autowired
+    private TemplateObjectProService templateObjectProService;
+    /**
+     * 根据业务对象rowId查找当前对象下的所有属性并分页显示
+     *
+     * @param args     按照空格查询
+     * @param pageNum  当前第几页
+     * @param pageSize 一页显示多少条
+     * @param request  request请求
+     * @param locale   国际化参数
+     * @return ServiceResult
+     */
+    @RequestMapping("/queryProPage")
+    public Object queryProPage(String rowId,
+                               String args,
+                               @RequestParam(value = "pageNum", defaultValue = BaseConstants.PAGE_NUM) int pageNum,
+                               @RequestParam(value = "pageSize", defaultValue = BaseConstants.PAGE_SIZE) int pageSize,
+                               String order,
+                               HttpServletRequest request,
+                               Locale locale) {
+        LinkedList<Order> str = UtilsTool.dataSort(order);
+        PageResult<Map<String, Object>> result = null;
+
+        if (UtilsTool.isValid(args)) {
+            result = templateObjectProService.select(
+                    new ConditionBuilder(TemplateObjectPro.class).and()
+                            .equal("templateObjRowId", rowId).or()
+                            .addCondition(UtilsTool.createBlankQuery(Arrays.asList("code", "cname","ename"),
+                                    UtilsTool.collectToSet(args))).endOr().endAnd().buildDone()
+                    , Arrays.asList(QueryAction.ALL_FIELD), str, pageNum, pageSize);
+            if (result.getResult().size() == 0) {
+                return super.result(request, ServiceResult.Msg(PlatResult.Msg(BaseConstants.STATUS_FAIL, Message.QUERY_FAIL)), locale);
+            }
+            return super.result(request, ServiceResult.Msg(new PlatResult(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, result)), locale);
+        }
+        result =
+                templateObjectProService.select(
+                        new ConditionBuilder(TemplateObjectPro.class).and()
+                                .equal("templateObjRowId", rowId).endAnd().buildDone()
+                        , Arrays.asList(QueryAction.ALL_FIELD), str, pageNum, pageSize);
+        if (result.getResult().size() == 0) {
+            return super.result(request, ServiceResult.Msg(PlatResult.Msg(BaseConstants.STATUS_FAIL,Message.QUERY_FAIL)), locale);
+        }
+        return super.result(request, ServiceResult.Msg(new PlatResult(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, result)), locale);
+    }
+
 }
--- src/main/java/com/bcx/plat/core/controller/TemplateObjectProController.java
+++ src/main/java/com/bcx/plat/core/controller/TemplateObjectProController.java
@@ -1,15 +1,28 @@
 package com.bcx.plat.core.controller;
 
+import com.bcx.plat.core.base.BaseConstants;
 import com.bcx.plat.core.common.BaseControllerTemplate;
+import com.bcx.plat.core.constants.Message;
+import com.bcx.plat.core.entity.BusinessObjectPro;
 import com.bcx.plat.core.entity.TemplateObject;
 import com.bcx.plat.core.entity.TemplateObjectPro;
+import com.bcx.plat.core.morebatis.builder.ConditionBuilder;
+import com.bcx.plat.core.morebatis.cctv1.PageResult;
+import com.bcx.plat.core.morebatis.command.QueryAction;
+import com.bcx.plat.core.morebatis.component.FieldCondition;
+import com.bcx.plat.core.morebatis.component.Order;
+import com.bcx.plat.core.morebatis.component.constant.Operator;
 import com.bcx.plat.core.service.TemplateObjectProService;
 import com.bcx.plat.core.service.TemplateObjectService;
+import com.bcx.plat.core.utils.PlatResult;
+import com.bcx.plat.core.utils.ServiceResult;
+import com.bcx.plat.core.utils.UtilsTool;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * <p>Title: TemplateObjectProController</p>
@@ -18,7 +31,7 @@ import java.util.List;
  *
  * @author Wen TieHu
  * @version 1.0
- *          <pre>Histroy:
+ *          <pre> Histroy:
  *                2017/8/28  Wen TieHu Create
  *          </pre>
  */
@@ -30,4 +43,7 @@ public class TemplateObjectProController extends BaseControllerTemplate<Template
     protected List<String> blankSelectFields() {
         return Arrays.asList("code","cname","ename");
     }
+
+
+
 }
