96ede5b63c4e949b87446915f1625bb90a06a25b
jimishu <243047281@qq.com>
2017-08-29 15:38:17 +0800

    键值集合页面调用公共方法

--- src/main/webapp/core/WebUI/key-value-set/key-value-set-add.js
+++ src/main/webapp/core/WebUI/key-value-set/key-value-set-add.js
@@ -4,100 +4,83 @@
 
 var keyValueSetAdd = new Vue({
     el: '#keyValueSetAdd',
-    data: function () {
-        return {
-            labelPosition: 'right',
-            keyForm:{
-                keysetCodeInput: '',
-                numberInput:'',
-                keysetNameInput: '',
-                confKeyInput: '',
-                confValueInput: '',
-                despInput: '',
-                versionInput: '',
-                disabled:true
-            },
-            rules: {
-                numberInput: [
-                    {required: true, message: '请输入编号', trigger: 'blur'},
-                ],
-                keysetNameInput: [
-                    {required: true, message: '请输入名称', trigger: 'blur'}
-                ],
-                confKeyInput: [
-                    {required: true, message: '请输入键', trigger: 'blur'}
-                ],
-                confValueInput: [
-                     {required: true, message: '请输入值', trigger: 'blur'}
-                ],
-                //despInput: [
-                //     {required: true, message: '请输入说明', trigger: 'blur'}
-                // ]
-            }
+    data: {
+        labelPosition: 'right',
+        addUrl:serverPath+'/keySet/add',
+        modifyUrl:serverPath+'/keySet/modify',
+        rowObj:'',
+        keyForm:{
+            keysetCodeInput: '',
+            numberInput:'',
+            keysetNameInput: '',
+            confKeyInput: '',
+            confValueInput: '',
+            despInput: '',
+            versionInput: '',
+            disabled:true
+        },
+        rules: {
+            numberInput: [
+                {required: true, message: '请输入编号', trigger: 'blur'},
+            ],
+            keysetNameInput: [
+                {required: true, message: '请输入名称', trigger: 'blur'}
+            ],
+            confKeyInput: [
+                {required: true, message: '请输入键', trigger: 'blur'}
+            ],
+            confValueInput: [
+                {required: true, message: '请输入值', trigger: 'blur'}
+            ],
+            //despInput: [
+            //     {required: true, message: '请输入说明', trigger: 'blur'}
+            // ]
         }
     },
     methods: {
+        //新增
+        addKeySet(){
+            gmpAjax.showAjax(keyValueSetAdd.addUrl,{
+                number:this.keyForm.numberInput,
+                keysetName: this.keyForm.keysetNameInput,
+                confKey: this.keyForm.confKeyInput,
+                confValue: this.keyForm.confValueInput,
+                desp: this.keyForm.despInput,
+            },function(res){
+                showMsg.MsgOk(keyValueSet,res);
+                queryData.getData(keyValueSet.url,keyValueSet.input,keyValueSet);
+                ibcpLayer.Close(topButtonObj.divIndex);
+            })
+        },
+
+        //编辑
+        editKeySet(rowId){
+            gmpAjax.showAjax(keyValueSetAdd.modifyUrl,{
+                rowId:rowId,
+                number:this.keyForm.numberInput,
+                keysetName: this.keyForm.keysetNameInput,
+                confKey: this.keyForm.confKeyInput,
+                confValue: this.keyForm.confValueInput,
+                desp: this.keyForm.despInput,
+            },function(res){
+                showMsg.MsgOk(keyValueSet,res);
+                queryData.getData(keyValueSet.url,keyValueSet.input,keyValueSet);
+                ibcpLayer.Close(keyValueSet.editdivIndex);
+            })
+        },
+
         confirm(formName) {
-            //var datas = [
-            //    this.$refs.numberInput,
-            //    this.$refs.keysetNameInput,
-            //    this.$refs.confKeyInput,
-            //    this.$refs.confValueInput,
-            //];
-            //for (var i = 0; i < datas.length; i++) {
-            //    if (datas[i].value == '') {
-            //        ibcpLayer.ShowMsg(datas[i].placeholder);
-            //        return;
-            //    }
-            //}
             this.$refs[formName].validate(function (valid) {
-                if (valid) {
-                    if (operate == 1) {
-                        addObj.addOk(function(){
-                            keyValueSetAdd.$http.jsonp(insert, {
-                                //keysetCode: keyValueSetAdd.keyForm.keysetCodeInput,
-                                number:keyValueSetAdd.keyForm.numberInput,
-                                keysetName: keyValueSetAdd.keyForm.keysetNameInput,
-                                confKey: keyValueSetAdd.keyForm.confKeyInput,
-                                confValue: keyValueSetAdd.keyForm.confValueInput,
-                                desp: keyValueSetAdd.keyForm.despInput,
-                                //version: keyValueSetAdd.version
-                            }, {
-                                jsonp: 'callback'
-                            }).then(function (res) {
-                                showMsg.MsgOk(keyValueSet,res)
-                                keyValueSet.input='';
-                                queryData.getData(queryPage,keyValueSet.input,keyValueSet,function(res){});
-                                //keyValueSet.search();
-                                ibcpLayer.Close(divIndex);
-                            });
-                        },function(){
-                            showMsg.MsgError(keyValueSet)
+                if(topButtonObj.isEdit == true){//编辑
+                    if(valid){
+                        editObj.editOk(function(){
+                            keyValueSetAdd.editKeySet(keyValueSetAdd.rowObj.rowId);
                         })
                     }
-                    if (operate == 2) {
-                        editObj.editOk(function(){
-                            keyValueSetAdd.$http.jsonp(modify, {
-                                //拿到这条数据的ID
-                                rowId: keyValueSet.currentVal.rowId,
-                                //keysetCode: keyValueSetAdd.keysetCodeInput,
-                                number:keyValueSetAdd.keyForm.numberInput,
-                                keysetName: keyValueSetAdd.keyForm.keysetNameInput,
-                                confKey: keyValueSetAdd.keyForm.confKeyInput,
-                                confValue: keyValueSetAdd.keyForm.confValueInput,
-                                desp: keyValueSetAdd.keyForm.despInput,
-                                // version: keyValueSetAdd.version
-                            }, {
-                                jsonp: 'callback'
-                            }).then(function (res) {
-                                showMsg.MsgOk(keyValueSet,res)
-                                keyValueSet.input='';
-                                //keyValueSet.search();
-                                queryData.getData(queryPage,keyValueSet.input,keyValueSet,function(res){});
-                                ibcpLayer.Close(divIndex);
-                            });
-                        },function(){
-                            showMsg.MsgError(keyValueSet)
+                }else{//新增
+                    if(valid){
+                        addObj.addOk(function(){
+                            keyValueSetAdd.addKeySet();
                         })
                     }
                 }
