5c61885215beb7a224ba8ad50ff1944980603e7a
jimishu <243047281@qq.com>
2017-08-31 16:18:30 +0800

    键值结合页面布局修改为左右结构，以及增删改查功能

--- src/main/webapp/core/WebUI/key-value-set/key-value-set.js
+++ src/main/webapp/core/WebUI/key-value-set/key-value-set.js
@@ -1,55 +1,82 @@
 /**
  * Created by jms on 2017/8/6.
  */
+var url=serverPath+'/keySet/queryPage';
+var ProUrl=serverPath+'/keySetPro/queryPage';
+var queryByIdUrl=serverPath+'/keySet/queryById';
+var queryProUrl=serverPath+'/keySet/queryProPage';
+var queryProByIdUrl=serverPath+'/keySetPro/queryById'
+var delUrl=serverPath+'/keySet/delete';
+var delProUrl=serverPath+'/keySetPro/delete'
 
-var keyValueSet=new Vue({
-    el:"#kvsInfo",
+/*
+ * 左边table
+ */
+var leftKeyValueSet=new Vue({
+    el:"#left",
     data: getData.dataObj({
-        "rowObj":'',
-        "divIndex":'',
-        "editdivIndex":'',
-        url:serverPath+'/keySet/queryPage',
-        delUrl:serverPath+'/keySet/delete'
-
+        divIndex:'',
+        editdivIndex:'',
     }),
     methods: {
-        searchPage(){
-            pagingObj.Example(this.url,this.input, this.pageSize,this.pageNum,this,function(){});
+        //点击查询按钮
+        leftSearch(){
+            this.searchLeftPage();
         },
-        search(){
-             this.searchPage();
-        },
-        //点击某行
-        currentChange(row, event, column){
-            this.rowObj = row;
-        },
-        editEvent(){
-            operate = 2;
-            var htmlUrl = 'key-value-set-add.html';
-            this.editdivIndex = ibcpLayer.ShowDiv(htmlUrl, '编辑键值集合', '400px', '520px', function () {
-                //code值
-                topButtonObj.isEdit=true;
-                keyValueSet.input='';
-                keyValueSetAdd.rowObj=keyValueSet.rowObj;
-                keyValueSetAdd.keyForm.keysetCodeInput = keyValueSet.rowObj.keysetCode;
-                keyValueSetAdd.keyForm.numberInput=keyValueSet.rowObj.number;
-                keyValueSetAdd.keyForm.keysetNameInput = keyValueSet.rowObj.keysetName;
-                keyValueSetAdd.keyForm.confKeyInput = keyValueSet.rowObj.confKey;
-                keyValueSetAdd.keyForm.confValueInput = keyValueSet.rowObj.confValue;
-                keyValueSetAdd.keyForm.despInput = keyValueSet.rowObj.desp;
-                keyValueSetAdd.keyForm.versionInput = keyValueSet.rowObj.version;
+        //分页查询方法
+        searchLeftPage(){
+            pagingObj.Example(url,this.input, this.pageSize,this.pageNum,this,function(){
+                leftKeyValueSet.leftCurrentChange(leftKeyValueSet.tableData[0]);
             });
         },
-        deleteEvent(){
+        //点击某行
+        leftCurrentChange(row, event, column){
+            if(row){
+                this.rowId=row.rowId;
+                rightKeyValueSet.getRightData(row.rowId);
+            }
+        },
+
+        //左侧编辑
+        editLeftEvent(res){
+            var htmlUrl = 'key-value-set-add.html';
+            this.editdivIndex = ibcpLayer.ShowDiv(htmlUrl, '编辑主信息', '400px', '500px', function () {
+                var data={
+                    "url":queryByIdUrl,
+                    "jsonData":{
+                        rowId:leftKeyValueSet.rowId,
+                    },
+                    "obj":leftKeyValueSet
+                }
+                gmpAjax.showAjax(data,function(res){
+                    var data=res;
+                    topButtonObj.isEdit=true;
+                    keyValueSetAdd.keyForm.keysetCodeInput =data[0].keysetCode;
+                    keyValueSetAdd.keyForm.keysetNameInput = data[0].keysetName;
+                    keyValueSetAdd.keyForm.belongModuleInput = data[0].belongModule;
+             //       keyValueSetAdd.keyForm.belongSystemInput = data[0].belongSystem;
+                    keyValueSetAdd.keyForm.despInput = data[0].desp;
+                    keyValueSetAdd.keyForm.versionInput = data[0].version;
+                })
+            });
+        },
+        //左侧删除
+        deleteLeftEvent(){
             deleteObj.del(function () {
-                gmpAjax.showAjax(keyValueSet.delUrl,
-                    {rowId:keyValueSet.rowObj.rowId},
-                    function(res){
-                    showMsg.MsgOk(keyValueSet,res);
-                    queryData.getData(keyValueSet.url,keyValueSet.input,keyValueSet)
+                var data = {
+                    "url": delUrl,
+                    "jsonData": {
+                        rowId: leftKeyValueSet.rowId
+                    },
+                    "obj": leftKeyValueSet
+                }
+                gmpAjax.showAjax(data, function (res) {
+                    queryData.getData(url, leftKeyValueSet.input, leftKeyValueSet, function (res) {
+                        leftKeyValueSet.leftCurrentChange(leftKeyValueSet.tableData[0]);
+                        // rightKeyValueSet.getRightData(leftKeyValueSet.tableData[0].rowId);
+                    })
                 })
             })
-
         },
         FindFirstDate(row){
             this.$refs.tableData.setCurrentRow(row); //将选中的行变颜色
@@ -64,11 +91,114 @@ var keyValueSet=new Vue({
         },
 
         headSort(column){//列头排序
-            pagingObj.headSort(queryPage,this.input,this.pageSize,this.pageNum,column,this);
+            pagingObj.headSort(url,this.input,this.pageSize,this.pageNum,column,this);
         }
     },
     created(){
-        this.searchPage();
+        //初始化分页查询
+        this.searchLeftPage();
+        $(document).ready(function(){
+            leftKeyValueSet.leftHeight=$(window).height()-190;
+        });
+        $(window).resize(function(){
+            leftKeyValueSet.leftHeight=$(window).height()-190;
+        })
+    },
+    updated() {
+        //默认选中第一行
+        this.FindFirstDate(this.tableData[0]);
+    }
+})
+
+var rightKeyValueSet=new Vue({
+    el:"#right",
+    data: getData.dataObj({
+        rightHeight:'',
+        rightInput:'',
+        editProDivIndex:'',
+        rowId:''
+    }),
+    methods: {
+        searchPage(){
+            pagingObj.Examples(ProUrl,leftKeyValueSet.rowId,this.rightInput, this.pageSize,this.pageNum,this,function(){
+                rightKeyValueSet.rightCurrentChange(rightKeyValueSet.tableData[0]);
+            });
+        },
+        rightSearch(){
+            this.searchPage();
+        },
+        //点击某行
+        rightCurrentChange(row, event, column){
+            if(row){
+                this.rowId=row.rowId;
+            }
+        },
+        //右边table查询
+        getRightData(id){
+            pagingObj.Examples(queryProUrl,id,this.rightInput,this.pageSize,this.pageNum,this,function(){
+                rightKeyValueSet.rightCurrentChange(rightKeyValueSet.tableData[0]);
+            });
+        },
+        //右边表格编辑
+        editRightEvent(){
+            var htmlUrl = 'key-value-set-pro-add.html';
+            this.editProDivIndex = ibcpLayer.ShowDiv(htmlUrl, '编辑明细信息', '400px', '400px', function () {
+                var data={
+                    "url":queryProByIdUrl,
+                    "jsonData":{
+                        rowId:rightKeyValueSet.rowId,
+                    },
+                    "obj":rightKeyValueSet
+                }
+                gmpAjax.showAjax(data,function(res){
+                    // showMsg.MsgOk(keyValueSet,res);
+                    //code值
+                    var data=res;
+                    topButtonObj.isEdit=true;
+                    keyValueSetProAdd.keyForm.confKeyInput = data[0].confKey;
+                    keyValueSetProAdd.keyForm.confValueInput = data[0].confValue;
+                    keyValueSetProAdd.keyForm.despInput = data[0].desp;
+                })
+            });
+        },
+        deleteRigthEvent(){
+            deleteObj.del(function () {
+                var data = {
+                    "url": delProUrl,
+                    "jsonData": {
+                        rowId: rightKeyValueSet.rowId
+                    },
+                    "obj": rightKeyValueSet
+                }
+                gmpAjax.showAjax(data, function (res) {
+                    queryData.getData(ProUrl, rightKeyValueSet.rightInput, leftKeyValueSet.rowId,this);
+                    leftKeyValueSet.searchLeftPage();
+                })
+            })
+        },
+        FindFirstDate(row){
+            this.$refs.tableData.setCurrentRow(row); //将选中的行变颜色
+        },
+        handleSizeChange(val){//每页显示多少条
+            this.pageSize=val;
+            this.searchPage();
+        },
+        handleCurrentChange(val){//点击第几页
+            this.pageNum=val;
+            this.searchPage();
+        },
+
+        headSort(column){//列头排序
+            pagingObj.headSort(ProUrl,this.rightInput,this.pageSize,this.pageNum,column,this);
+        }
+    },
+    created(){
+        $(document).ready(function(){
+            rightKeyValueSet.rightHeight=$(window).height()-190;
+        });
+        $(window).resize(function(){
+            rightKeyValueSet.rightHeight=$(window).height()-190;
+        })
     },
     updated() {
         this.FindFirstDate(this.tableData[0]);
@@ -76,6 +206,9 @@ var keyValueSet=new Vue({
 })
 
 
+/*
+ * 按钮
+ */
 var topButtonObj = new Vue({
     el:'#myButton',
     data:{
@@ -85,12 +218,18 @@ var topButtonObj = new Vue({
     methods:{
         addEvent(){
             var htmlUrl = 'key-value-set-add.html';
-            this.divIndex = ibcpLayer.ShowDiv(htmlUrl, '新增键值集合', '400px', '520px',function(){
+            this.divIndex = ibcpLayer.ShowDiv(htmlUrl, '新增主信息', '400px', '500px',function(){
                 topButtonObj.isEdit=false;
-                keyValueSet.input='';
+                leftKeyValueSet.input='';
+            });
+        },
+        addProEvent(){
+            var htmlUrl = 'key-value-set-pro-add.html';
+            this.divIndex = ibcpLayer.ShowDiv(htmlUrl, '新增明细', '400px', '300px',function(){
+                topButtonObj.isEdit=false;
+                rightKeyValueSet.rightInput='';
             });
         }
     }
 })
-
 
