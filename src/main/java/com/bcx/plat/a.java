ef83f71e3a9d370014d3b76e083d999283c98b53
wth <wentiehu@batchsight.com>
2017-09-03 15:32:23 +0800

    业务逻辑加而且判断

---
+++ src/main/java/com/bcx/plat/a.java
@@ -0,0 +1,15 @@
+package com.bcx.plat;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Shanghai Batchsight GMP Information of management platform, Inc. Copyright(c) 2017</p>
+ *
+ * @author Wen TieHu
+ * @version 1.0
+ *          <pre>Histroy:
+ *                2017/9/3  Wen TieHu Create
+ *          </pre>
+ */
+public class a {
+}
