20142880b1c0b5116fd620867dc3d35049007fe2
HCL <825944942@qq.com>
2017-09-04 17:30:14 +0800

    增加需求

--- src/main/java/com/bcx/plat/core/base/BaseORM.java
+++ src/main/java/com/bcx/plat/core/base/BaseORM.java
@@ -18,4 +18,30 @@ public abstract class BaseORM<T extends BeanInterface> implements BeanInterface<
     return (MoreBatis) SpringContextHolder.getBean("moreBatis");
   }
 
+  /**
+   * Mr 汪，我需要你实现这些方法 -.-
+   */
+
+  // TODO int insert Mr汪，实现这个方法
+
+  // TODO int deleteById() 删除当前这条数据
+  // TODO int deleteByCondition(condition) 根据条件删除
+
+  // TODO int update(condition) 更新数据     ！！！空字符串更新，null不更新
+  // TODO int updateAll(condition)  更新所有数据  包括 null
+  // TODO int updateById() 更新数据，根据主键，主键无效不更新       ！！！空字符串更新，null不更新
+  // TODO int updateAllById()  更新所有数据，主键无效不更新      包括 null
+
+  // TODO int delete(condition)
+  // TODO int deleteAll()
+  // TODO int deleteById
+
+  // TODO List<T> selectAll()
+  // TODO List<T> selectOne(condition)
+  // TODO List<T> selectList(condition)
+  // TODO Page<List<T>> selectPage(page,condition)
+  // TODO T selectById()
+  // TODO T selectById(id)
+  // TODO int selectCount(condition)
+
 }
