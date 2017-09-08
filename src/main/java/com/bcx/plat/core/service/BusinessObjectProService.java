f775da4a39c2090eb00572d26b146f39bee743d1
HCL <825944942@qq.com>
2017-09-08 19:31:23 +0800

    修正Service错误

--- src/main/java/com/bcx/plat/core/service/BusinessObjectProService.java
+++ src/main/java/com/bcx/plat/core/service/BusinessObjectProService.java
@@ -1,22 +1,8 @@
 package com.bcx.plat.core.service;
 
-import com.bcx.plat.core.base.BaseConstants;
 import com.bcx.plat.core.base.BaseService;
-import com.bcx.plat.core.constants.Message;
-import com.bcx.plat.core.entity.*;
-import com.bcx.plat.core.morebatis.app.MoreBatis;
-import com.bcx.plat.core.morebatis.component.FieldCondition;
-import com.bcx.plat.core.morebatis.component.constant.JoinType;
-import com.bcx.plat.core.morebatis.component.constant.Operator;
-import com.bcx.plat.core.utils.ServerResult;
-import com.bcx.plat.core.utils.UtilsTool;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bcx.plat.core.entity.BusinessObjectPro;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 业务对象
@@ -25,7 +11,7 @@ import java.util.Map;
 @Service
 public class BusinessObjectProService extends BaseService<BusinessObjectPro> {
 
-    private final DBTableColumnService dbTableColumnService;
+    /*private final DBTableColumnService dbTableColumnService;
     private final MoreBatis moreBatis;
 
     @Autowired
@@ -35,12 +21,12 @@ public class BusinessObjectProService extends BaseService<BusinessObjectPro> {
     }
 
 
-    /**
+    *//**
      * 根据业务对象属性rowId查询当前数据
      *
      * @param rowId 唯一标识
      * @return PlatResult
-     */
+   *//*
     public ServerResult queryById(String rowId) {
         List<BusinessObjectPro> businessObjectPros = select(new FieldCondition("rowId", Operator.EQUAL, rowId));
         if (!UtilsTool.isValid(businessObjectPros)) {
@@ -63,57 +49,57 @@ public class BusinessObjectProService extends BaseService<BusinessObjectPro> {
         return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, dbTableColumns);
     }
 
-    /**
+    *//**
      * 供前端功能块属性使用
      * 根据业务对象rowId查询当前业务对象下的所有属性
      *
      * @param objRowId   业务对象唯一标识
      * @param frontRowId 功能块唯一标识
      * @return ServerResult
-     */
+   *//*
     public ServerResult queryBusinPro(String objRowId, String frontRowId) {
         //通过业务对象查找到对应的模板的rowId
         String templateRowId;
         List<Map<String, Object>> execu;
         List<Map<String, Object>> results = new ArrayList<>();
-        /*
+        *//*
             根据业务对象rowId查询出和业务对象属性关联的属性
             这里的业务对象的属性是基本属性
-         */
+         *//*
         List<Map<String, Object>> result = moreBatis.select(BusinessObject.class, BusinessObjectPro.class, "rowId", "objRowId", JoinType.LEFT_JOIN)
                 .where(new FieldCondition(moreBatis.getColumnByAlias(BusinessObjectPro.class, "objRowId"), Operator.EQUAL, objRowId)).execute();
-        /*
+        *//*
             遍历添加数据到results
-         */
+         *//*
         for (Map<String, Object> res : result) {
             res.put("attrSource", BaseConstants.ATTRIBUTE_SOURCE_BASE);
             results.add(res);
         }
-        /*
+        *//*
             根据业务对象rowId来查询关联表数据,获取到关联业务对象的模板对象的rowId
-         */
+         *//*
         List<Map<String, Object>> execute = moreBatis.select(BusinessObject.class, BusinessRelateTemplate.class,
                 "rowId", "businessRowId", JoinType.LEFT_JOIN)
                 .where(new FieldCondition(moreBatis.getColumnByAlias(BusinessRelateTemplate.class, "businessRowId"), Operator.EQUAL, objRowId)).execute();
-        /*
+        *//*
             将返回的结果下划线转为驼峰
-         */
+         *//*
         List<Map<String, Object>> list = UtilsTool.underlineKeyMapListToCamel(execute);
-        /*
+        *//*
         遍历关联表中的结果,获取关联的模板对象的rowId
-         */
+         *//*
         for (Map<String, Object> ex : list) {
             //通过业务对象查找到对应的模板对象的rowId
             templateRowId = ex.get("templateRowId").toString();
-            /*
+            *//*
             通过获取到的模板对象的rowId来查询出模板对象关联的模板对象属性的数据
-             */
+             *//*
             execu = moreBatis.select(TemplateObject.class, TemplateObjectPro.class, "rowId", "templateObjRowId", JoinType.LEFT_JOIN)
                     .where(new FieldCondition(moreBatis.getColumnByAlias(TemplateObjectPro.class, "templateObjRowId"), Operator.EQUAL, templateRowId)).execute();
             List<Map<String, Object>> exe = UtilsTool.underlineKeyMapListToCamel(execu);
-            /*
+            *//*
             将查询出来的模板对象属性的数据,遍历获取数据,拿到代码和名称,将结果塞到results结果里面
-             */
+             *//*
             for (Map<String, Object> es : exe) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("propertyCode", es.get("code").toString());
@@ -159,7 +145,7 @@ public class BusinessObjectProService extends BaseService<BusinessObjectPro> {
             return ServerResult.Msg(BaseConstants.STATUS_FAIL, Message.QUERY_FAIL);
         }
         return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, UtilsTool.underlineKeyMapListToCamel(results));
-    }
+    }*/
 
 
 }
--- src/main/java/com/bcx/plat/core/service/KeySetService.java
+++ src/main/java/com/bcx/plat/core/service/KeySetService.java
@@ -1,23 +1,8 @@
 package com.bcx.plat.core.service;
 
-import com.bcx.plat.core.base.BaseConstants;
 import com.bcx.plat.core.base.BaseService;
-import com.bcx.plat.core.constants.Message;
 import com.bcx.plat.core.entity.KeySet;
-import com.bcx.plat.core.entity.KeySetPro;
-import com.bcx.plat.core.morebatis.app.MoreBatis;
-import com.bcx.plat.core.morebatis.command.QueryAction;
-import com.bcx.plat.core.morebatis.component.FieldCondition;
-import com.bcx.plat.core.morebatis.component.Order;
-import com.bcx.plat.core.morebatis.component.constant.JoinType;
-import com.bcx.plat.core.morebatis.component.constant.Operator;
-import com.bcx.plat.core.morebatis.phantom.Condition;
-import com.bcx.plat.core.utils.ServerResult;
-import com.bcx.plat.core.utils.UtilsTool;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 /**
  * 键值集合信息
@@ -26,7 +11,7 @@ import java.util.*;
 @Service
 public class KeySetService extends BaseService<KeySet> {
 
-  private final MoreBatis moreBatis;
+  /*private final MoreBatis moreBatis;
   private final KeySetProService keySetProService;
 
   @Autowired
@@ -36,12 +21,12 @@ public class KeySetService extends BaseService<KeySet> {
   }
 
 
-  /**
+  *//**
    * 根据编号number查询，以数组的形式传入数据进来
    *
    * @param list 搜索条件
    * @return PlatResult
-   */
+   *//*
   public ServerResult queryKeySet(List list) {
     Map<Object, Object> maps = new HashMap<>();
     for (Object li : list) {
@@ -58,12 +43,12 @@ public class KeySetService extends BaseService<KeySet> {
     return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, UtilsTool.objToJson(maps));
   }
 
-  /**
+  *//**
    * 根据键值集合编号查询对应的数据
    *
    * @param keyCode
    * @return
-   */
+   *//*
   public ServerResult queryKeyCode(String keyCode) {
     List<Map<String, Object>> result = moreBatis.select(KeySet.class)
             .where(new FieldCondition("keysetCode", Operator.EQUAL, keyCode))
@@ -82,12 +67,12 @@ public class KeySetService extends BaseService<KeySet> {
   }
 
 
-  /**
+  *//**
    * 根据rowId查询数据
    *
    * @param rowId 唯一标示
    * @return ServerResult
-   */
+   *//*
   public ServerResult<List<Map<String, Object>>> queryPro(String rowId) {
     List<Map> rowId1 =
             keySetProService.selectMap(new FieldCondition("relateKeysetRowId", Operator.EQUAL, rowId));
@@ -95,7 +80,7 @@ public class KeySetService extends BaseService<KeySet> {
   }
 
 
-  /**
+  *//**
    * 根据键值集合rowId查找当前键值集合属性并分页显示
    *
    * @param search   搜索条件
@@ -104,7 +89,7 @@ public class KeySetService extends BaseService<KeySet> {
    * @param pageSize 一页显示条数
    * @param orders   排序
    * @return ServerResult
-   */
+   *//*
   public ServerResult queryProPage(String search, String rowId, int pageNum, int pageSize, List<Order> orders) {
     Condition condition = null;
     if (UtilsTool.isValid(rowId)) {
@@ -115,12 +100,12 @@ public class KeySetService extends BaseService<KeySet> {
   }
 
 
-  /**
+  *//**
    * 根据rowId删除业务对象数据
    *
    * @param rowId 唯一标示
    * @return
-   */
+   *//*
   public ServerResult delete(String rowId) {
     List<Map> rkrd = keySetProService.selectMap(new FieldCondition("relateKeysetRowId", Operator.EQUAL, rowId));
     for (Map<String, Object> rk : rkrd) {
@@ -130,6 +115,6 @@ public class KeySetService extends BaseService<KeySet> {
     new KeySet().deleteById(rowId);
     return ServerResult.Msg(BaseConstants.STATUS_FAIL, Message.DATA_QUOTE);
 
-  }
+  }*/
 
 }
