264a83df5614689065d12dbce457ec40645e90a6
wjf <xeternity@qq.com>
2017-09-09 17:12:25 +0800

    修正单元测试

--- src/test/java/com/bcx/plat/core/morebatis/EntityTest.java
+++ src/test/java/com/bcx/plat/core/morebatis/EntityTest.java
@@ -34,9 +34,9 @@ public class EntityTest extends BaseTest {
     businessObject1.setRowId(rowId);
     businessObject1 = businessObject1.selectOneById();
     //查找与新增测试
-    Assert.assertTrue("插入失败", businessObject1.getObjectName().equals("testObject"));
-    Assert.assertTrue("map没有被插入到数据库", businessObject1.getEtc().get("a").equals(100));
-    Assert.assertTrue("map没有被插入到数据库", businessObject1.getEtc().get("b").equals("b"));
+    Assert.assertEquals("插入失败", businessObject1.getObjectName(),"testObject");
+    Assert.assertEquals("map没有被插入到数据库", businessObject1.getEtc().get("a"),100);
+    Assert.assertEquals("map没有被插入到数据库", businessObject1.getEtc().get("b"),"b");
 
     businessObject1.setObjectName("secondName");
     businessObject1.getEtc().put("a", "update");
@@ -50,15 +50,15 @@ public class EntityTest extends BaseTest {
     businessObject1.updateById();
     businessObject1.setObjectName("anotherName");
     businessObject1.setEtc(null);
-//    businessObject1.selectById();
+    businessObject1 = businessObject1.selectOneById();
     //更新测试
-    Assert.assertTrue("更新失败", businessObject1.getObjectName().equals("secondName"));
-    Assert.assertTrue("map没有被更新到数据库", businessObject1.getEtc().get("a").equals("update"));
-    Assert.assertTrue("map没有被更新到数据库", businessObject1.getEtc().get("b").equals("successed"));
+    Assert.assertEquals("更新失败", businessObject1.getObjectName(),"secondName");
+    Assert.assertEquals("map没有被更新到数据库", businessObject1.getEtc().get("a"),"update");
+    Assert.assertEquals("map没有被更新到数据库", businessObject1.getEtc().get("b"),"successed");
 
     businessObject1.deleteById();
     //删除测试
-    Assert.assertTrue("删除失败", businessObject1.selectOneById() == null);
+    Assert.assertEquals("删除失败", businessObject1.selectOneById(),null);
 //    Assert.assertTrue("删除失败", businessObject1.selectById() == null);
   }
 }
