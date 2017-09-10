97ec3134b1d4b9aa415bb96eca5eeaf06cef13dc
HCL <825944942@qq.com>
2017-09-10 13:26:45 +0800

    重构result的用法

--- src/main/java/com/bcx/plat/core/service/DBTableColumnService.java
+++ src/main/java/com/bcx/plat/core/service/DBTableColumnService.java
@@ -24,47 +24,48 @@ import java.util.Map;
 @Service
 public class DBTableColumnService extends BaseService<DBTableColumn> {
 
-    /**
-     * 通过表信息字段rowId查询表信息并分页显示
-     *
-     * @param search   搜索条件
-     * @param rowId    表字段rowId
-     * @param orders   排序条件
-     * @param pageNum  页码
-     * @param pageSize 一页显示多少条
-     * @return ServerResult
-     */
-    public ServerResult queryPageById(String search, String rowId, LinkedList<Order> orders, int pageNum, int pageSize) {
-        pageNum =UtilsTool.isValid(search)?1:pageNum;
-        PageResult<Map<String, Object>> relateTableRowId = selectPageMap(new And(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId),
-                        UtilsTool.createBlankQuery(blankSelectFields(), UtilsTool.collectToSet(search))),
-                orders, pageNum, pageSize);
-        return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, relateTableRowId);
-    }
+  /**
+   * 通过表信息字段rowId查询表信息并分页显示
+   *
+   * @param search   搜索条件
+   * @param rowId    表字段rowId
+   * @param orders   排序条件
+   * @param pageNum  页码
+   * @param pageSize 一页显示多少条
+   * @return ServerResult
+   */
+  public ServerResult queryPageById(String search, String rowId, LinkedList<Order> orders, int pageNum, int pageSize) {
+    pageNum = UtilsTool.isValid(search) ? 1 : pageNum;
+    PageResult<Map<String, Object>> relateTableRowId = selectPageMap(new And(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId),
+                    UtilsTool.createBlankQuery(blankSelectFields(), UtilsTool.collectToSet(search))),
+            orders, pageNum, pageSize);
+    return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, relateTableRowId);
+  }
 
-    /**
-     * 根据表信息的rowId来查询表字段中的信息
-     * @param rowId     表信息rowId
-     * @param search    搜索条件
-     * @return ServerResult
-     */
-    public ServerResult queryTableById(String rowId, String search) {
-        List<DBTableColumn> relateTableRowId = select(new And(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId),
-                UtilsTool.createBlankQuery(blankSelectFields(), UtilsTool.collectToSet(search))));
-        if (relateTableRowId.size() == 0) {
-            return ServerResult.Msg(BaseConstants.STATUS_FAIL, Message.QUERY_FAIL);
-        }
-        return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, relateTableRowId);
+  /**
+   * 根据表信息的rowId来查询表字段中的信息
+   *
+   * @param rowId  表信息rowId
+   * @param search 搜索条件
+   * @return ServerResult
+   */
+  public ServerResult queryTableById(String rowId, String search) {
+    List<DBTableColumn> relateTableRowId = select(new And(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId),
+            UtilsTool.createBlankQuery(blankSelectFields(), UtilsTool.collectToSet(search))));
+    if (relateTableRowId.size() == 0) {
+      return null;//  ServerResult.Msg(BaseConstants.STATUS_FAIL, Message.QUERY_FAIL);
     }
+    return new ServerResult<>(BaseConstants.STATUS_SUCCESS, Message.QUERY_SUCCESS, relateTableRowId);
+  }
 
 
-    /**
-     * 模糊搜索的条件
-     *
-     * @return 字段
-     */
-    protected List<String> blankSelectFields() {
-        return Arrays.asList("columnEname", "columnCname");
-    }
+  /**
+   * 模糊搜索的条件
+   *
+   * @return 字段
+   */
+  protected List<String> blankSelectFields() {
+    return Arrays.asList("columnEname", "columnCname");
+  }
 
 }
