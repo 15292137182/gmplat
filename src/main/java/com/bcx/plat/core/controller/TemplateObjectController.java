da667d50ba374419be8f8deee71a535591b49377
HCL <825944942@qq.com>
2017-09-15 19:15:29 +0800

    去除分页默认值

--- src/main/java/com/bcx/plat/core/controller/TemplateObjectController.java
+++ src/main/java/com/bcx/plat/core/controller/TemplateObjectController.java
@@ -129,8 +129,8 @@ public class TemplateObjectController extends BaseController {
    */
   @RequestMapping("/queryPage")
   public PlatResult singleInputSelect(String search,
-                                      @RequestParam(value = "pageNum", defaultValue = BaseConstants.PAGE_NUM) int pageNum,
-                                      @RequestParam(value = "pageSize", defaultValue = BaseConstants.PAGE_SIZE) int pageSize,
+                                      @RequestParam(value = "pageNum") int pageNum,
+                                      @RequestParam(value = "pageSize") int pageSize,
                                       String order) {
     LinkedList<Order> orders = dataSort(order);
     Or blankQuery = search.isEmpty() ? null : UtilsTool.createBlankQuery(blankSelectFields(), UtilsTool.collectToSet(search));
