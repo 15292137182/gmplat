a6b816b5cbe1e9790563569c4fcf6d72201c2d78
欧朝培 <ouyangguangfly@qq.com>
2017-09-18 14:23:36 +0800

    改实体类字段

--- src/main/java/com/bcx/plat/core/entity/BusinessObjectPro.java
+++ src/main/java/com/bcx/plat/core/entity/BusinessObjectPro.java
@@ -13,115 +13,114 @@ import static com.bcx.plat.core.utils.UtilsTool.lengthUUID;
  */
 public class BusinessObjectPro extends BaseEntity<BusinessObjectPro> implements Serializable {
 
+  private String propertyCode;//业务对象属性代码
+  private String propertyName;//业务对象属性名称
+  private String relateTableColumn;//关联表字段
+  private String valueType;//值类型
+  private String valueResourceType;//值来源类型
+  private String valueResourceContent;//值来源内容
+  private String wetherExpandPro;//是否为扩展属性
+  private String objRowId;//业务对象关联rowId
+  private String defaultValue;//默认值
+  private String fieldAlias;//字段别名
 
-    private String propertyCode;//业务对象属性代码
-    private String propertyName;//业务对象属性名称
-    private String relateTableColumn;//关联表字段
-    private String valueType;//值类型
-    private String valueResourceType;//值来源类型
-    private String valueResourceContent;//值来源内容
-    private String wetherExpandPro;//是否为扩展属性
-    private String objRowId;//业务对象关联rowId
-    private String defaultValue;//默认值
-    private String extendsEname;//扩展字段英文名
+  /**
+   * 构建 - 创建信息
+   *
+   * @return 返回自身
+   */
+  @Override
+  public BusinessObjectPro buildCreateInfo() {
+    setPropertyCode(SequenceManager.getInstance().buildSequenceNo(CodeMessage.BUSIN_OBJECT_PRO, null));
+    setRowId(lengthUUID(32));
+    return super.buildCreateInfo();
+  }
 
-    /**
-     * 构建 - 创建信息
-     *
-     * @return 返回自身
-     */
-    @Override
-    public BusinessObjectPro buildCreateInfo() {
-        setPropertyCode(SequenceManager.getInstance().buildSequenceNo(CodeMessage.BUSIN_OBJECT_PRO, null));
-        setRowId(lengthUUID(32));
-        return super.buildCreateInfo();
-    }
+  public String getFieldAlias() {
+    return fieldAlias;
+  }
 
-    public String getDefaultValue() {
-        return defaultValue;
-    }
+  public void setFieldAlias(String fieldAlias) {
+    this.fieldAlias = fieldAlias;
+  }
 
-    public void setDefaultValue(String defaultValue) {
-        this.defaultValue = defaultValue;
-    }
+  public String getDefaultValue() {
+    return defaultValue;
+  }
 
-    public String getExtendsEname() {
-        return extendsEname;
-    }
+  public void setDefaultValue(String defaultValue) {
+    this.defaultValue = defaultValue;
+  }
 
-    public void setExtendsEname(String extendsEname) {
-        this.extendsEname = extendsEname;
-    }
+  public String getObjRowId() {
+    return objRowId;
+  }
 
-    public String getObjRowId() {
-        return objRowId;
-    }
+  public void setObjRowId(String objRowId) {
+    this.objRowId = objRowId;
+  }
 
-    public void setObjRowId(String objRowId) {
-        this.objRowId = objRowId;
-    }
+  public String getRowId() {
+    return rowId;
+  }
 
-    public String getRowId() {
-        return rowId;
-    }
+  public void setRowId(String rowId) {
+    this.rowId = rowId;
+  }
 
-    public void setRowId(String rowId) {
-        this.rowId = rowId;
-    }
+  public String getPropertyCode() {
+    return propertyCode;
+  }
 
-    public String getPropertyCode() {
-        return propertyCode;
-    }
+  public void setPropertyCode(String propertyCode) {
+    this.propertyCode = propertyCode;
+  }
 
-    public void setPropertyCode(String propertyCode) {
-        this.propertyCode = propertyCode;
-    }
+  public String getPropertyName() {
+    return propertyName;
+  }
 
-    public String getPropertyName() {
-        return propertyName;
-    }
+  public void setPropertyName(String propertyName) {
+    this.propertyName = propertyName;
+  }
 
-    public void setPropertyName(String propertyName) {
-        this.propertyName = propertyName;
-    }
+  public String getRelateTableColumn() {
+    return relateTableColumn;
+  }
 
-    public String getRelateTableColumn() {
-        return relateTableColumn;
-    }
+  public void setRelateTableColumn(String relateTableColumn) {
+    this.relateTableColumn = relateTableColumn;
+  }
 
-    public void setRelateTableColumn(String relateTableColumn) {
-        this.relateTableColumn = relateTableColumn;
-    }
+  public String getValueType() {
+    return valueType;
+  }
 
-    public String getValueType() {
-        return valueType;
-    }
+  public void setValueType(String valueType) {
+    this.valueType = valueType;
+  }
 
-    public void setValueType(String valueType) {
-        this.valueType = valueType;
-    }
+  public String getValueResourceType() {
+    return valueResourceType;
+  }
 
-    public String getValueResourceType() {
-        return valueResourceType;
-    }
+  public void setValueResourceType(String valueResourceType) {
+    this.valueResourceType = valueResourceType;
+  }
 
-    public void setValueResourceType(String valueResourceType) {
-        this.valueResourceType = valueResourceType;
-    }
+  public String getValueResourceContent() {
+    return valueResourceContent;
+  }
 
-    public String getValueResourceContent() {
-        return valueResourceContent;
-    }
+  public void setValueResourceContent(String valueResourceContent) {
+    this.valueResourceContent = valueResourceContent;
+  }
 
-    public void setValueResourceContent(String valueResourceContent) {
-        this.valueResourceContent = valueResourceContent;
-    }
+  public String getWetherExpandPro() {
+    return wetherExpandPro;
+  }
 
-    public String getWetherExpandPro() {
-        return wetherExpandPro;
-    }
-
-    public void setWetherExpandPro(String wetherExpandPro) {
-        this.wetherExpandPro = wetherExpandPro;
-    }
+  public void setWetherExpandPro(String wetherExpandPro) {
+    this.wetherExpandPro = wetherExpandPro;
+  }
 }
