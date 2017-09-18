b11fbd2f5d4b0b1782080486287abe3cab2f1322
欧朝培 <ouyangguangfly@qq.com>
2017-09-18 20:30:05 +0800

    数据库表信息管理，删除功能bug修改

--- src/main/java/com/bcx/plat/core/service/MaintDBTablesService.java
+++ src/main/java/com/bcx/plat/core/service/MaintDBTablesService.java
@@ -25,40 +25,39 @@ import java.util.stream.Collectors;
  */
 @Service
 public class MaintDBTablesService extends BaseService<MaintDBTables> {
-    @Autowired
-    private BusinessObjectService businessObjectService;
-    @Autowired
-    private DBTableColumnService dbTableColumnService;
+  @Autowired
+  private BusinessObjectService businessObjectService;
+  @Autowired
+  private DBTableColumnService dbTableColumnService;
 
-    /**
-     * 根据维护表信息rowId删除数据
-     *
-     * @param rowId 唯一标识
-     * @return ServerResult
-     */
-    public ServerResult delete(String rowId) {
-        ServerResult serverResult = new ServerResult();
-        AtomicReference<Map<String, Object>> map = new AtomicReference<>(new HashMap<>());
-        if (UtilsTool.isValid(rowId)) {
-            map.get().put("relateTableRowId", rowId);
-            List<Map> relateTableRowId = businessObjectService.selectMap(new FieldCondition("relateTableRowId"
-                    , Operator.EQUAL, rowId));
-            if (relateTableRowId.size() == 0) {
-                List<Map> list = dbTableColumnService.selectMap(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId));
-                if (UtilsTool.isValid(list)) {
-                    List<String> rowIds = list.stream().map((row) ->
-                            (String) row.get("rowId")).collect(Collectors.toList());
-                    ConditionBuilder conditionBuilder = new ConditionBuilder(MaintDBTables.class);
-                    conditionBuilder.and().equal("rowId", rowIds);
-                    new DBTableColumn().buildDeleteInfo().delete(new FieldCondition("rowId", Operator.EQUAL, rowIds));
-                } else {
-                    MaintDBTables maintDBTables = new MaintDBTables().buildDeleteInfo();
-                    maintDBTables.delete(new FieldCondition("rowId", Operator.EQUAL, rowId));
-                    return serverResult.setStateMessage(BaseConstants.STATUS_SUCCESS, Message.DELETE_SUCCESS);
-                }
-            }
+  /**
+   * 根据维护表信息rowId删除数据
+   *
+   * @param rowId 唯一标识
+   * @return ServerResult
+   */
+  public ServerResult delete(String rowId) {
+    ServerResult serverResult = new ServerResult();
+    AtomicReference<Map<String, Object>> map = new AtomicReference<>(new HashMap<>());
+    if (UtilsTool.isValid(rowId)) {
+      map.get().put("relateTableRowId", rowId);
+      List<Map> relateTableRowId = businessObjectService.selectMap(new FieldCondition("relateTableRowId"
+          , Operator.EQUAL, rowId));
+      if (relateTableRowId.size() == 0) {
+        List<Map> list = dbTableColumnService.selectMap(new FieldCondition("relateTableRowId", Operator.EQUAL, rowId));
+        if (UtilsTool.isValid(list)) { // 如果有，先删除关联信息
+          List<String> rowIds = list.stream().map((row) ->
+              (String) row.get("rowId")).collect(Collectors.toList());
+          ConditionBuilder conditionBuilder = new ConditionBuilder(MaintDBTables.class);
+          conditionBuilder.and().equal("rowId", rowIds);
+          new DBTableColumn().buildDeleteInfo().delete(new FieldCondition("rowId", Operator.IN, rowIds));
         }
-        return serverResult.setStateMessage(BaseConstants.STATUS_FAIL, Message.DATA_QUOTE);
+        MaintDBTables maintDBTables = new MaintDBTables().buildDeleteInfo();
+        maintDBTables.delete(new FieldCondition("rowId", Operator.EQUAL, rowId));
+        return serverResult.setStateMessage(BaseConstants.STATUS_SUCCESS, Message.DELETE_SUCCESS);
+      }
     }
+    return serverResult.setStateMessage(BaseConstants.STATUS_FAIL, Message.DATA_QUOTE);
+  }
 
 }
