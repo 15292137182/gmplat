feb0d533b315abce095d10442ff7c1646aaad767
Zhang Yangmei <zhangyangmei@batchsight.com>
2017-09-22 10:53:28 +0800

    模板对象验证

--- src/main/webapp/core/WebUI/template-objects/template-add-prop.js
+++ src/main/webapp/core/WebUI/template-objects/template-add-prop.js
@@ -1,73 +1,101 @@
 /**
  * Created by admin on 2017/8/27.
  */
-
+var count=true;
 var addTempProp = new Vue({
     "el": "#addEventProp",
     data: function () {
             return {
                 labelPosition: 'right',
                 addTempPropObj: {
-                    codeInput: '',
-                    engNameInput: '',
-                    chnNameInput: '',
-                    default:'',
-                    comContent:''
+                    codeInput: '',//代码
+                    engNameInput: '',//英文名
+                    chnNameInput: '',//中文名
+                    default:'',//默认值,
+                    typeValue:'',//值类型
+                    comContent:''//说明
                 },
-                value_1:'',
                 valueType_1:{
                     params:"valueType",
                     value:"",
                     disabled:"false"
-                }
+                },
+                //验证
+                rules: {
+                    engNameInput: [
+                        { required: true,message: '请输入英文名'}
+                    ],
+                    chnNameInput: [
+                        { required: true, message: '请输入中文名'},
+                    ],
+                    default: [
+                        { required: true,message: '请输入默认值'}
+                    ],
+                    typeValue: [
+                        { required: true,trigger: 'blur', message: '请选择值类型'},
+                    ],
+                    comContent: [
+                        { required: true,message: '请输入说明'}
+                    ]
+                },
             }
         },
     methods: {
         //保存事件
-        saveTempProp(){
-            if(operateOPr==1){
-                addObj.addOk(function(){
-                    var data={
-                        "url":addObjTemp,
-                        "jsonData":{templateObjRowId:basLeft.currentId,
-                            ename:addTempProp.addTempPropObj.engNameInput,
-                            cname:addTempProp.addTempPropObj.chnNameInput,
-                            valueType:addTempProp.valueType_1.value,
-                            defaultValue:addTempProp.addTempPropObj.default,
-                            desp:addTempProp.addTempPropObj.comContent
-                        },
-                        "obj":basRight
-                    };
-                    gmpAjax.showAjax(data,function(res){
-                        //关闭弹层
-                        ibcpLayer.Close(divIndex);
-                        //分页查询
-                        queryData.getDatas(queryObjTemp,basRight.input,basLeft.currentId,basRight,function(res){})
-                    })
-                })
-            }else if(operateOPr==2){
-                editObj.editOk(function(){
-                    var data={
-                        "url":editObjTemp,
-                        "jsonData":{templateObjRowId:basLeft.currentId,
-                            proRowId:basRight.currentProId,
-                            ename:addTempProp.addTempPropObj.engNameInput,
-                            cname:addTempProp.addTempPropObj.chnNameInput,
-                            valueType:addTempProp.valueType_1.value,
-                            defaultValue:addTempProp.addTempPropObj.default,
-                            desp:addTempProp.addTempPropObj.comContent
-                        },
-                        "obj":basRight
-                    };
-                    gmpAjax.showAjax(data,function(res){
-                        //关闭弹层
-                        ibcpLayer.Close(divIndex);
-                        //分页查询
-                        queryData.getDatas(queryObjTemp,basRight.input,basLeft.currentId,basRight,function(res){
+        saveTempProp(formName){
+            this.$refs.addTempPropObj.validate((valid) => {
+                if(valid){
+                    if(operateOPr==1){
+                        addObj.addOk(function(){
+                            var data={
+                                "url":addObjTemp,
+                                "jsonData":{templateObjRowId:basLeft.currentId,
+                                    ename:addTempProp.addTempPropObj.engNameInput,
+                                    cname:addTempProp.addTempPropObj.chnNameInput,
+                                    valueType:addTempProp.valueType_1.value,
+                                    defaultValue:addTempProp.addTempPropObj.default,
+                                    desp:addTempProp.addTempPropObj.comContent
+                                },
+                                "obj":basRight
+                            };
+                            gmpAjax.showAjax(data,function(res){
+                                //关闭弹层
+                                ibcpLayer.Close(divIndex);
+                                //分页查询
+                                queryData.getDatas(queryObjTemp,basRight.input,basLeft.currentId,basRight,function(res){})
+                            })
                         })
-                    })
-                })
-            }
+                    }else if(operateOPr==2){
+                        editObj.editOk(function(){
+                            var data={
+                                "url":editObjTemp,
+                                "jsonData":{templateObjRowId:basLeft.currentId,
+                                    proRowId:basRight.currentProId,
+                                    ename:addTempProp.addTempPropObj.engNameInput,
+                                    cname:addTempProp.addTempPropObj.chnNameInput,
+                                    valueType:addTempProp.valueType_1.value,
+                                    defaultValue:addTempProp.addTempPropObj.default,
+                                    desp:addTempProp.addTempPropObj.comContent
+                                },
+                                "obj":basRight
+                            };
+                            gmpAjax.showAjax(data,function(res){
+                                //关闭弹层
+                                ibcpLayer.Close(divIndex);
+                                //分页查询
+                                queryData.getDatas(queryObjTemp,basRight.input,basLeft.currentId,basRight,function(res){
+                                })
+                            })
+                        })
+                    }
+                }
+               else {
+                    $('.el-form-item__error').css("display", "block");
+                    addTempProp.judgeMent();
+                    return false;
+                }
+            })
+
         },
         //取消事件
         cancelTempProp(){
@@ -76,7 +104,24 @@ var addTempProp = new Vue({
         //值类型下拉框事件
         getValueType_1(datas){
             this.valueType_1.value = datas.value;
-        }
+
+            this.addTempPropObj.typeValue = datas.value;
+            var a=this.$refs.valueType_1.$children[0].$children[0].$el;
+            var b=$(a).children('input');
+            if(this.addTempPropObj.typeValue.length==0) {
+                if(count!=true){
+                    $(b).css('borderColor', '#ff4949');
+                }
+                count=false;
+            }else{
+                $(b).css('borderColor','#bfcbd9');
+            }
+        },
+        judgeMent(){
+            setTimeout(function(){
+                $('.el-form-item__error').css("display", "none");
+            },1500)
+        },
 
     }
 });
--- src/main/webapp/core/WebUI/template-objects/template-add.js
+++ src/main/webapp/core/WebUI/template-objects/template-add.js
@@ -1,64 +1,85 @@
 /**
  * Created by admin on 2017/8/27.
  */
-
+var count=true;
 var addTemp = new Vue({
     "el": "#addEvent",
     data: function () {
             return {
                 labelPosition: 'right',
                 addTempObj: {
-                    codeInput: '',
-                    nameInput: '',
-                    comContent: '',
-                    system:'',
+                    codeInput: '',//代码
+                    nameInput: '',//名称
+                    modules:'',//模块
+                    comContent: '',//说明
                 },
                 //所属模块下拉框
                 belongModule_1:{
                     params:'belongModule',
                     value:'',
                     disabled:'false',
-                }
+                },
+                //验证
+                rules: {
+                    nameInput: [
+                        { required: true,message: '请输入名称'}
+                    ],
+                    modules: [
+                        { required: true,trigger: 'blur', message: '请选择模块'},
+                    ],
+                    comContent: [
+                        { required: true,message: '请输入说明'}
+                    ]
+                },
             }
     },
     methods: {
         //保存事件
-        saveTemp(){
-            //新增
-            if(operate==1){
-                addObj.addOk(function(){
-                    var data={
-                        "url":addTempObj,
-                        "jsonData":{templateName:addTemp.addTempObj.nameInput,desp:addTemp.addTempObj.comContent,belongModule:addTemp.belongModule_1.value,
-                            belongSystem:addTemp.addTempObj.system},
-                        "obj":basLeft
-                    };
-                    gmpAjax.showAjax(data,function(res){
-                        //关闭弹层
-                        ibcpLayer.Close(divIndex);
-                        //分页查询
-                        queryData.getData(queryTemp,basLeft.input,basLeft,function(res){
-                            basLeft.currentChange(basLeft.tableData[0]);
+        saveTemp(formName){
+            this.$refs.addTempObj.validate((valid) => {
+                if (valid) {
+                    //新增
+                    if(operate==1){
+                        addObj.addOk(function(){
+                            var data={
+                                "url":addTempObj,
+                                "jsonData":{templateName:addTemp.addTempObj.nameInput,desp:addTemp.addTempObj.comContent,belongModule:addTemp.belongModule_1.value,
+                                    belongSystem:addTemp.addTempObj.system},
+                                "obj":basLeft
+                            };
+                            gmpAjax.showAjax(data,function(res){
+                                //关闭弹层
+                                ibcpLayer.Close(divIndex);
+                                //分页查询
+                                queryData.getData(queryTemp,basLeft.input,basLeft,function(res){
+                                    basLeft.currentChange(basLeft.tableData[0]);
+                                })
+                            })
                         })
-                    })
-                })
-            }if(operate==2){
-                editObj.editOk(function(){
-                    var data={
-                        "url":editTempObj,
-                        "jsonData":{templateCode:addTemp.addTempObj.codeInput,rowId:basLeft.currentId,templateName:addTemp.addTempObj.nameInput,desp:addTemp.addTempObj.comContent,
-                            belongModule:addTemp.belongModule_1.value, belongSystem:addTemp.addTempObj.system},
-                        "obj":basLeft
-                    };
-                    gmpAjax.showAjax(data,function(res){
-                        console.log(res)
-                        ibcpLayer.Close(divIndex);
-                        queryData.getData(queryTemp,basLeft.input,basLeft,function(res){
-                            basLeft.currentChange(basLeft.tableData[0]);
+                    }if(operate==2){
+                        editObj.editOk(function(){
+                            var data={
+                                "url":editTempObj,
+                                "jsonData":{templateCode:addTemp.addTempObj.codeInput,rowId:basLeft.currentId,templateName:addTemp.addTempObj.nameInput,desp:addTemp.addTempObj.comContent,
+                                    belongModule:addTemp.belongModule_1.value, belongSystem:addTemp.addTempObj.system},
+                                "obj":basLeft
+                            };
+                            gmpAjax.showAjax(data,function(res){
+                                console.log(res)
+                                ibcpLayer.Close(divIndex);
+                                queryData.getData(queryTemp,basLeft.input,basLeft,function(res){
+                                    basLeft.currentChange(basLeft.tableData[0]);
+                                })
+                            })
                         })
-                    })
-                })
-            }
+                    }
+                }else {
+                    $('.el-form-item__error').css("display", "block");
+                    addTemp.judgeMent();
+                    return false;
+                }
+            })
+
         },
         //取消事件
         cancelTemp(){
@@ -67,7 +88,23 @@ var addTemp = new Vue({
         //模块下拉框
         getBelongModule_1(datas){
             this.belongModule_1.value=datas.value;
-        }
 
+            this.addTempObj.modules = datas.value;
+            var a=this.$refs.belongModule_1.$children[0].$children[0].$el;
+            var b=$(a).children('input');
+            if(this.addTempObj.modules.length==0) {
+                if(count!=true){
+                    $(b).css('borderColor', '#ff4949');
+                }
+                count=false;
+            }else{
+                $(b).css('borderColor','#bfcbd9');
+            }
+        },
+        judgeMent(){
+            setTimeout(function(){
+                $('.el-form-item__error').css("display", "none");
+            },1500)
+        },
     }
 });
