dffac71a9d9ae837a3114f8eadeac057a97b1833
欧朝培 <ouyangguangfly@qq.com>
2017-09-28 15:39:19 +0800

    去除冗余

--- src/main/java/com/bcx/plat/core/morebatis/app/MoreBatis.java
+++ src/main/java/com/bcx/plat/core/morebatis/app/MoreBatis.java
@@ -9,10 +9,11 @@ import com.bcx.plat.core.morebatis.component.Field;
 import com.bcx.plat.core.morebatis.component.constant.JoinType;
 import com.bcx.plat.core.morebatis.phantom.FieldSource;
 import com.bcx.plat.core.morebatis.phantom.TableSource;
+import org.springframework.stereotype.Component;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import org.springframework.stereotype.Component;
 
 @Component
 public interface MoreBatis {
@@ -22,39 +23,39 @@ public interface MoreBatis {
    *
    * @param entityClass 实体类class
    */
-  public Collection<Field> getPks(Class entityClass);
+  Collection<Field> getPks(Class entityClass);
 
   /**
    * 获取实体类全部字段
    *
    * @param entityClass 实体类class
    */
-  public Collection<Field> getColumns(Class entityClass);
+  Collection<Field> getColumns(Class entityClass);
 
   /**
    * 获取实体类对应表
    *
    * @param entityClass 实体类class
    */
-  public TableSource getTable(Class entityClass);
+  TableSource getTable(Class entityClass);
 
-  public Field getColumnByAlias(Class entityClass, String alias);
+  Field getColumnByAlias(Class entityClass, String alias);
 
   /**
    * 根据实体类的class对象与实体类属性名称获取对应字段对象 如果没有对应的字段就返回扩展属性
    *
    * @param entityClass 实体类
-   * @param alias 实体类属性名称
+   * @param alias       实体类属性名称
    */
-  public FieldSource getColumnOrEtcByAlias(Class entityClass, String alias);
+  FieldSource getColumnOrEtcByAlias(Class entityClass, String alias);
 
   /**
    * 根据实体类的class对象与实体类属性名称获取对应字段对象
    *
    * @param entityClass 实体类
-   * @param alias 实体类属性名称
+   * @param alias       实体类属性名称
    */
-  public Collection<Field> getColumnByAlias(Class entityClass, Collection<String> alias);
+  Collection<Field> getColumnByAlias(Class entityClass, Collection<String> alias);
 
   /**
    * 插入一个实体类
@@ -63,7 +64,7 @@ public interface MoreBatis {
    * @return 返回插入的结果标识
    */
 //  @Deprecated
-  public <T extends BeanInterface<T>> int insertEntity(T entity);
+  <T extends BeanInterface<T>> int insertEntity(T entity);
 
   /**
    * 根据主键删除一个实体类
@@ -71,7 +72,7 @@ public interface MoreBatis {
    * @param entity 要更新的实体类
    */
 //  @Deprecated
-  public <T extends BeanInterface<T>> int deleteEntity(T entity);
+  <T extends BeanInterface<T>> int deleteEntity(T entity);
 
   /**
    * 根据主键更新一个实体类
@@ -79,7 +80,7 @@ public interface MoreBatis {
    * @param entity 要更新的实体类
    */
 //  @Deprecated
-  public <T extends BeanInterface<T>> int updateEntity(T entity);
+  <T extends BeanInterface<T>> int updateEntity(T entity);
 
   /**
    * 根据主键更新一个实体类
@@ -87,7 +88,7 @@ public interface MoreBatis {
    * @param entity 要更新的实体类
    */
 //  @Deprecated
-  public <T extends BeanInterface<T>> int updateEntity(T entity, Object excluded);
+  <T extends BeanInterface<T>> int updateEntity(T entity, Object excluded);
 
 
   /**
@@ -96,50 +97,50 @@ public interface MoreBatis {
    * @param entity 要更新的实体类
    */
 //  @Deprecated
-  public <T extends BeanInterface<T>> int updateEntity(T entity, Collection excluded);
+  <T extends BeanInterface<T>> int updateEntity(T entity, Collection excluded);
 
   /**
    * 单表查询
    *
    * @param entity 要查询的实体类class
    */
-  public QueryAction select(Class entity);
+  QueryAction select(Class entity);
 
   /**
    * 两个表的inner join查询
    *
-   * @param primary 主表class对象
-   * @param secondary 从表class对象
-   * @param relationPrimary 主表中与从表关联的字段
+   * @param primary           主表class对象
+   * @param secondary         从表class对象
+   * @param relationPrimary   主表中与从表关联的字段
    * @param relationSecondary 从表中与主表关联的字段
    * @return 未设置条件的查询语句对象
    */
-  public QueryAction select(Class primary,
-      Class secondary, String relationPrimary, String relationSecondary);
+  QueryAction select(Class primary,
+                     Class secondary, String relationPrimary, String relationSecondary);
 
 
   /**
    * 两个表join查询
    *
-   * @param primary 主表class对象
-   * @param secondary 从表class对象
-   * @param relationPrimary 主表中与从表关联的字段
+   * @param primary           主表class对象
+   * @param secondary         从表class对象
+   * @param relationPrimary   主表中与从表关联的字段
    * @param relationSecondary 从表中与主表关联的字段
-   * @param joinType join类型
+   * @param joinType          join类型
    * @return 未设置条件的查询语句对象
    */
-  public QueryAction select(Class primary,
-      Class secondary, String relationPrimary, String relationSecondary, JoinType joinType);
+  QueryAction select(Class primary,
+                     Class secondary, String relationPrimary, String relationSecondary, JoinType joinType);
 
-  public QueryAction selectStatement();
+  QueryAction selectStatement();
 
   /**
    * 更新操作（未设置条件）
    *
    * @param entity 实体类的对象
-   * @param value 要更新的值(支持null)
+   * @param value  要更新的值(支持null)
    */
-  public UpdateAction update(Class entity, Map<String, Object> value);
+  UpdateAction update(Class entity, Map<String, Object> value);
 
   /**
    * 多行插入
@@ -147,16 +148,16 @@ public interface MoreBatis {
    * @param entity 实体类的class
    * @param values 插入数据库的多行数据
    */
-  public InsertAction insert(Class entity, List<Map<String, Object>> values);
+  InsertAction insert(Class entity, List<Map<String, Object>> values);
 
   /**
    * 单行插入
    *
    * @param entity 实体类的class
-   * @param value 插入数据库的单行数据
+   * @param value  插入数据库的单行数据
    * @return 未执行的插入语句对象
    */
-  public InsertAction insert(Class entity, Map<String, Object> value);
+  InsertAction insert(Class entity, Map<String, Object> value);
 
   /**
    * 删除(未设置条件)
@@ -164,19 +165,19 @@ public interface MoreBatis {
    * @param entity 实体类的class
    * @return 未设置条件的删除语句对象
    */
-  public DeleteAction delete(Class entity);
+  DeleteAction delete(Class entity);
 
-  public InsertAction insertStatement();
+  InsertAction insertStatement();
 
-  public DeleteAction deleteStatement();
+  DeleteAction deleteStatement();
 
-  public List<Map<String, Object>> execute(QueryAction queryAction);
+  List<Map<String, Object>> execute(QueryAction queryAction);
 
-  public int execute(InsertAction insertAction);
+  int execute(InsertAction insertAction);
 
-  public int execute(UpdateAction updateAction);
+  int execute(UpdateAction updateAction);
 
-  public int execute(DeleteAction deleteAction);
+  int execute(DeleteAction deleteAction);
 
   String getDefaultMapColumnAlias();
 }
