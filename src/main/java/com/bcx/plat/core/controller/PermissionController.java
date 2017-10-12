507421ea7e275f53ac9254aab9f7d75255e0aa52
HCL <825944942@qq.com>
2017-10-12 20:55:25 +0800

    权限服务

---
+++ src/main/java/com/bcx/plat/core/controller/PermissionController.java
@@ -0,0 +1,19 @@
+package com.bcx.plat.core.controller;
+
+import com.bcx.plat.core.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.bcx.plat.core.constants.Global.PLAT_SYS_PREFIX;
+
+/**
+ * 权限控制器
+ * <p>
+ * Create By HCL at 2017/10/12
+ */
+@RestController
+@RequestMapping(PLAT_SYS_PREFIX + "/core/permission")
+public class PermissionController extends BaseController {
+
+
+}
--- src/main/java/com/bcx/plat/core/service/PermissionService.java
+++ src/main/java/com/bcx/plat/core/service/PermissionService.java
@@ -1,8 +1,12 @@
 package com.bcx.plat.core.service;
 
 import com.bcx.plat.core.base.BaseService;
+import com.bcx.plat.core.constants.Message;
 import com.bcx.plat.core.entity.Permission;
+import com.bcx.plat.core.utils.ServerResult;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * 权限服务类
@@ -10,5 +14,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PermissionService extends BaseService<Permission> {
+
+  /**
+   * 插入数据，数据来自 map
+   *
+   * @param entity 实体数据
+   * @return 返回操作信息
+   */
+  public ServerResult insertEntityMap(Map<String, Object> entity) {
+    if (null != entity) {
+
+    }
+    return fail(Message.INVALID_REQUEST);
+  }
 
 }
