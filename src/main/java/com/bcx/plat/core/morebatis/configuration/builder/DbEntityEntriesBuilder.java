89acbaa41f9967ee25c9305128c77e06d61d8ac3
wjf <xeternity@qq.com>
2017-10-19 10:30:39 +0800

    业务对象无效条件增加空字符串

--- src/main/java/com/bcx/plat/core/morebatis/configuration/builder/DbEntityEntriesBuilder.java
+++ src/main/java/com/bcx/plat/core/morebatis/configuration/builder/DbEntityEntriesBuilder.java
@@ -9,6 +9,7 @@ import com.bcx.plat.core.morebatis.app.MoreBatis;
 import com.bcx.plat.core.morebatis.component.Field;
 import com.bcx.plat.core.morebatis.component.FieldCondition;
 import com.bcx.plat.core.morebatis.component.Table;
+import com.bcx.plat.core.morebatis.component.condition.And;
 import com.bcx.plat.core.morebatis.component.constant.Operator;
 import com.bcx.plat.core.morebatis.configuration.EntityEntry;
 import com.bcx.plat.core.morebatis.phantom.Condition;
@@ -20,19 +21,22 @@ import com.bcx.plat.core.entity.*; //这个导入不可以删除
 import java.util.stream.Collectors;
 
 public class DbEntityEntriesBuilder implements EntityEntriesBuilder {
+
   @Override
   public List<EntityEntry> getEntries(MoreBatis moreBatis) {
 //    ConditionBuilder conditionBuilder=new ConditionBuilder(BusinessObject.class);
 //    Condition condition = conditionBuilder.and()
 //        .notEqual("className", null).endAnd().buildDone();
-    Condition condition=new FieldCondition("className",Operator.IS_NULL,null).not();
-    List<Map<String, Object>> businessObjects = moreBatis.select(BusinessObject.class).where(condition).execute();
+    Condition condition = new FieldCondition("className", Operator.IS_NULL, null).not();
+    condition = new And(condition,new FieldCondition("className", Operator.EQUAL, "").not());
+    List<Map<String, Object>> businessObjects = moreBatis.select(BusinessObject.class)
+        .where(condition).execute();
     List<String> tableRowIds = businessObjects.stream().map(map -> {
-                                  return (String) map.get("relateTableRowId");
-                                }).collect(Collectors.toList());
+      return (String) map.get("relateTableRowId");
+    }).collect(Collectors.toList());
     List<String> objRowIds = businessObjects.stream().map(map -> {
-                                  return (String) map.get("rowId");
-                                }).collect(Collectors.toList());
+      return (String) map.get("rowId");
+    }).collect(Collectors.toList());
     List<Map<String, Object>> tables = moreBatis.select(MaintDBTables.class)
         .where(new FieldCondition("rowId", Operator.IN, tableRowIds)).execute();
     Map<String, Map<String, Object>> tablesMap = tables.stream().collect(Collectors.toMap(table -> {
@@ -64,6 +68,15 @@ public class DbEntityEntriesBuilder implements EntityEntriesBuilder {
           (String) tableMap.get("tableEname"));
       final List<Field> fields = new LinkedList<>();
       final List<Field> pks = new LinkedList<>();
+      /***模板属性预备代码***/
+//      LinkedList templates = UtilsTool
+//          .jsonToObj((String) businessObject.get("relateTemplateObject"), LinkedList.class,
+//              String.class);
+//      List<Map<String, Object>> templateRows = moreBatis.select(TemplateObjectPro.class)
+//          .where(new FieldCondition(
+//              moreBatis.getColumnByAlias(TemplateObjectPro.class, "templateObjRowId"), Operator.IN,
+//              templates)).execute();
+      /***模板属性预备代码***/
       objProps.stream().forEach(objProp -> {
         final String columnRowId = (String) objProp.get("relateTableColumn");
         final Map<String, Object> column = columnMap.get(columnRowId);
@@ -73,13 +86,14 @@ public class DbEntityEntriesBuilder implements EntityEntriesBuilder {
         Field field = new Field(table, fieldName, alias);
         fields.add(field);
         Integer isPkRaw = (Integer) column.get("isPk");
-        final boolean isPk = isPkRaw!=null && isPkRaw == 1 ? true : false;
+        final boolean isPk = isPkRaw != null && isPkRaw == 1 ? true : false;
         if (isPk) {
           pks.add(field);
         }
       });
       try {
-        return new EntityEntry((Class<? extends BeanInterface>) Class.forName((String) businessObject.get("className")), table, fields, pks);
+        return new EntityEntry((Class<? extends BeanInterface>) Class
+            .forName((String) businessObject.get("className")), table, fields, pks);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
         return null;
