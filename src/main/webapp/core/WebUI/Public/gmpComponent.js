084db9174511d0633475c09ca3a6af951d0eb9b3
liyuanquan <522258409@qq.com>
2017-10-20 20:45:25 +0800

    - 异步请求编辑获取默认选中树节点

--- src/main/webapp/core/WebUI/Public/gmpComponent.js
+++ src/main/webapp/core/WebUI/Public/gmpComponent.js
@@ -719,6 +719,49 @@ Vue.component("select-tree", {
                     self.middle = _selectNode;
                 }
             });
+        },
+        defaultCheckedKeys(val, oldVal) {
+            // console.log(val);
+            // console.log(oldVal);
+            var that = this;
+            // 获取树节点数据
+            this.getNode(function(data) {
+                // 获取配置信息默认选择项
+                var _checked = [];
+                // 获取选中节点
+                _checked.push(val);
+                // 获取配置信息显示名称
+                var _name = that.initial.defaultProps.label;
+                var _key = that.initial.defaultProps.key;
+                // 配置信息复选框
+                var _checkbox = that.checkbox;
+                // 选择节点信息
+                var _selectNode = [];
+                // 若配置信息中含有默认选择项
+                if(_checked && _checked.length > 0 && _checkbox) {
+                    // 遍历后端数据
+                    for(var i = 0;i < data.length;i++) {
+                        // 后端数据中 id为配置id的项 push到数组中
+                        for(var j = 0;j < _checked.length;j++) {
+                            if(data[i][_key] == _checked[j]) {
+                                _selectNode.push(data[i][_name]);
+                            }
+                        }
+                    }
+                    // 显示选中项名称
+                    that.select_node = _selectNode;
+                }else if(_checked && _checked.length > 0 && !_checkbox) {
+                    // 若不显示复选框 设置选中多节点 默认选中第一个配置节点
+                    for(var i = 0;i < data.length;i++) {
+                        // 后端数据中 id为配置id的项 push到数组中
+                        if(data[i][_key] == _checked[0]) {
+                            _selectNode.push(data[i][_name]);
+                        }
+                    }
+                    that.select_node = _selectNode;
+                    that.middle = _selectNode;
+                }
+            });
         }
     },
     methods: {
@@ -846,8 +889,10 @@ Vue.component("select-tree", {
         expanded(bool) {
             // 当前选择节点信息
             var _select = this.select_node;
+            // 配置信息checkbox
+            var _checkbox = this.initial.checkbox;
             // bool = true 为展开  bool = false 为收缩
-            if(bool && this.clearable) {
+            if(bool && this.clearable && _checkbox) {
                 // 获取配置展开项 选择项
                 var _expanded = this.initial.expanded;
                 var _checked = this.defaultCheckedKeys;
@@ -870,7 +915,7 @@ Vue.component("select-tree", {
                     // 确认按钮不可用
                     this.error = true;
                 }
-            }else if(!bool  && this.clearable) {
+            }else if(!bool && this.clearable && _checkbox) {
                 $("#gmpDrop .el-input").find('.el-input__icon').removeClass('is-reverse');
                 // 若选中节点没有确认
                 if(_select.length == 0) {
@@ -900,6 +945,8 @@ Vue.component("select-tree", {
                     // 确认按钮不可用
                     this.error = true;
                 }
+            }else if(!_checkbox) {
+                this.error = false;
             }
         },
         // 组织层级关系数据
@@ -1098,7 +1145,8 @@ Vue.component("select-tree", {
         // 监听窗口事件 下拉框宽度自适应
         window.addEventListener("resize", this.widthChange);
     },
-    updated() {
+    beforeUpdate() {
+        this.defaultCheckedKeys = this.initial.checked;
         // var self = this;
         // // 获取树节点数据
         // this.getNode(function(data) {
